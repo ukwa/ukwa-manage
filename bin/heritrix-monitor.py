39a8a58996f6b7fb125037d2bbf87e16e9a121ed
Roger G. Coram <roger.coram@bl.uk>
2013-11-26 11:15:39 +0000

    Heritrix-montoring script; fixes.

---
+++ bin/heritrix-monitor.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python2.7
+
+import heritrix
+from twitter import *
+
+ports = { "8443", "8444" }
+
+def send_message( port ):
+	t = Twitter( auth=OAuth( "16067043-PDJQCHs8JvjFbE0R98KBS8mOmgjboiDGecX8DzIwY", "PRb6uzccTe08mjOP565mCR3pZDxRxDyCSqCWfOhjg", "QQLmIMllFWxZKi0rdmPuSg", "Q6yGMQWBAlxgai3B45G9C8Na9i5k3bQ8hZ9j7JorIs" ) )
+	t.direct_messages.new( user="PsypherPunk", text="Could not connect to Heritrix on port " + port )
+
+for port in ports:
+	api = heritrix.API( host="https://opera.bl.uk:" + port + "/engine", user="admin", passwd="bl_uk", verbose=False, verify=False )
+	try:
+		api.rescan()
+	except Exception:
+		send_message( port )
+	
