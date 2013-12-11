8668299a6e06f47b8d652e9a1c05f26601467959
Roger G. Coram <roger.coram@bl.uk>
2013-12-11 11:19:21 +0000

    Added CREATE operation; changed path-handling.

--- webhdfs.py
+++ webhdfs.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import sys
 import json
 import logging
 import requests
 
 LOGGING_FORMAT="[%(asctime)s] %(levelname)s: %(message)s"
-logging.basicConfig( format=LOGGING_FORMAT, level=logging.DEBUG )
+logging.basicConfig( format=LOGGING_FORMAT, level=logging.WARNING )
 logger = logging.getLogger( "webhdfs" )
 
 class API():
@@ -19,25 +20,44 @@ class API():
 		r = requests.get( url, stream=stream )
 		return r
 
-	def list( self, path="/" ):
+	def _post( self, path, input=None, op="CREATE" ):
+		url = "%s%s?user.name=%s&op=%s&data=true" % ( self.prefix, path, self.user, op )
+		headers = { "content-type": "application/octet-stream" }
+		with open( input, "rb" ) as i:
+			r = requests.put( url, headers=headers, data=i )
+		return r
+
+	def list( self, path ):
 		return self._get( path=path, op="LISTSTATUS" )
 
-	def open( self, path="/" ):
+	def open( self, path ):
 		return self._get( path=path, op="OPEN" )
 
-	def openstream( self, path="/" ):
+	def openstream( self, path ):
 		return self._get( path=path, op="OPEN", stream=True )
 
-	def isdir( self, path="/" ):
+	def exists( self, path ):
 		r = self._get( path=path )
 		j = json.loads( r.text )
-		return j[ "FileStatus" ][ "type" ] == "DIRECTORY"
+		return j[ "FileStatus" ] is not None
 
-	def download( self, output, path="/" ):
-		r = openstream( path=path )
+	def isdir( self, path ):
+		r = self._get( path=path )
+		j = json.loads( r.text )
+		return ( self.exists( path ) and j[ "FileStatus" ][ "type" ] == "DIRECTORY" )
+
+	def download( self, path, output=sys.stdout ):
+		r = self.openstream( path )
 		with open( output, "wb" ) as o:
 			for chunk in r.iter_content( chunk_size=4096 ):
 				if chunk:
 					o.write( chunk )
 					o.flush()
+
+	def create( self, path, file=None ):
+		if file is None:
+			logger.warning( "Need file parameter." )
+		else:
+			r = self._post( path, input=file )
+			return r
 
