90919f67edb8cdc79e91ea8ee21157a50854050a
Roger G. Coram <roger.coram@bl.uk>
2014-01-06 10:54:08 +0000

    First attempt at a package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name="python-webhdfs",
+	version="0.0.1",
+	author="PsypherPunk",
+	author_email="psypherpunk@gmail.com",
+	packages=[ "webhdfs" ],
+	license="LICENSE.txt",
+	description="WebHDFS wrapper.",
+	long_description=open( "README.md" ).read(),
+	install_requires=[
+		"requests >= 2.0.0",
+	],
+)
