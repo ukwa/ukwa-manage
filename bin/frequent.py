ba4f599b20ca838d1127bfdc3b64f03ff5a3e8b4
Roger G. Coram <roger.coram@bl.uk>
2014-01-17 09:32:08 +0000

    Fixes to frequent jobs; Instances now added.

--- bin/frequent.py
+++ bin/frequent.py
@@ -1,8 +1,10 @@
-#!/usr/local/bin/python2.7
+#!/usr/bin/env python
 
 import os
+import act
 import sys
 import glob
+import json
 import time
 import shutil
 import httplib
@@ -11,6 +13,8 @@ import urllib2
 import logging
 import argparse
 import heritrix
+import StringIO
+import operator
 import subprocess32
 import dateutil.parser
 from lxml import etree
@@ -18,7 +22,7 @@ from settings import *
 from warcindexer import *
 from retry_decorator import *
 from datetime import datetime
-from collections import Counter
+from collections import Counter, OrderedDict
 from hanzo.warctools import WarcRecord
 from requests.exceptions import ConnectionError
 
@@ -35,6 +39,7 @@ logging.basicConfig( format=LOGGING_FORMAT, level=logging.DEBUG )
 logger = logging.getLogger( "frequency" )
 
 global api
+global now
 
 class Seed:
 	def tosurt( self, url ):
@@ -71,9 +76,6 @@ def waitfor( job, status ):
 def killRunningJob( newjob ):
 	for job in api.listjobs():
 		if job.startswith( newjob ) and api.status( job ) != "":
-# TODO: Enable the below to render jobs before killing them.
-#			logger.info( "Checking completeness for job: " + job )
-#			checkCompleteness( job, api.launchid( job ) )
 			logger.info( "Killing alread-running job: " + job )
 			api.pause( job )
 			waitfor( job, "PAUSED" )
@@ -138,12 +140,13 @@ def ignoreRobots( seeds, job ):
 			logger.info( "Ignoring robots.txt for SURT " + seed.surt )
 	return script
 
-def submitjob( newjob, seeds, frequency ):
+def submitjob( newjob, seeds ):
 	verifyApi()
 	killRunningJob( newjob )
 	root = setupjobdir( newjob )
 
 	#Set up profile, etc.
+	frequency = newjob.split( "-" )[ 0 ]
 	profile = HERITRIX_PROFILES + "/profile-frequent.cxml"
 	if not os.path.exists( profile ):
 		logger.error( "Cannot find profile for " + frequency + " [" + profile + "]" )
@@ -196,6 +199,7 @@ def check_frequencies():
 	for frequency in frequencies:
 		SEED_FILE = "/heritrix/" + frequency + "-seeds.txt"
 		seeds = []
+		global now
 		now = datetime.now()
 		if args.timestamp is not None:
 			now = dateutil.parser.parse( args.timestamp ).replace( tzinfo=None )
@@ -275,16 +279,53 @@ def check_frequencies():
 				jobname = frequency + "-" + now.strftime( "%m%d%H" ) + "00"
 			global api
 			api = heritrix.API( host="https://opera.bl.uk:" + heritrix_ports[ frequency ] + "/engine", user="admin", passwd="bl_uk", verbose=False, verify=False )
-			started_jobs.append( jobname )
-			submitjob( jobname, seeds, frequency )
+			started_jobs.append( ( jobname, seeds ) )
+			#TODO 0: Don't start the jobs here.
+			submitjob( jobname, seeds )
 	return started_jobs
 
-def checkCompleteness( job, launchid ):
-	logger.info( "Checking job " + job )
+def generate_wayback_indexes( job, launchid ):
+	logger.info( "Generating CDX for %s" % job )
 	warcs = glob.glob( WARC_ROOT + "/" + job + "/" + launchid + "/*.warc.gz*" )
 	logger.info( "Indexing " + str( len( warcs ) ) + " WARCs." )
 	index_warcs( warcs, QA_CDX, base_cdx=CDX )
+	logger.info( "Generating resource index for %s" % job )
 	generate_path_index( warcs, PATH_INDEX )
+
+def phantomjs_render( urls_to_render ):
+	logger.info( "Rendering " + str( len( urls_to_render ) ) + " URLs." )
+	timestamp = datetime.now().strftime( "%Y%m%d%H%M%S" )
+	output = ""
+	#TODO: Wayback should be running in proxy mode.
+	for url in urls_to_render:
+		try:
+			output += subprocess32.check_output( [ PHANTOMJS, NETSNIFF, WAYBACK + timestamp + "/" + url ], timeout=20 )
+		except ( subprocess32.CalledProcessError, subprocess32.TimeoutExpired ):
+			pass
+	return output
+
+#TODO: Move this externally?
+def migrate_to_ukwa( job, launchid, host ):
+	logger.info( "Migrating %s" % job )
+	generate_wayback_indexes( job, launchid )
+	crawl_logs = glob.glob( LOG_ROOT + "/" + job + "/" + launchid + "/crawl.log*" )
+	urls_to_render = []
+	for crawl_log in crawl_logs:
+		with open( crawl_log, "rb" ) as l:
+			for line in l:
+				url = line.split()[ 3 ]
+				if host in url:
+					urls_to_render.append( url )
+	output = phantomjs_render( urls_to_render )
+	urls_io = StringIO.StringIO( output )
+	for url in io.readlines():
+		cdx_lines = subprocess.check_output( [ "look", canonicalize( url ) + " ", QA_CDX ] )
+		cdx_io = StringIO.StringIO( cdx_lines )
+		#TODO: Check for revisits
+
+def checkCompleteness( job, launchid ):
+	logger.info( "Checking job %s" % job )
+	generate_wayback_indexes( job, launchid )
 	crawl_logs = glob.glob( LOG_ROOT + "/" + job + "/" + launchid + "/crawl.log*" )
 	urls_to_render = []
 	for crawl_log in crawl_logs:
@@ -294,23 +335,17 @@ def checkCompleteness( job, launchid ):
 				discovery_path = line.split()[ 4 ]
 				if url.startswith( "http" ) and len( discovery_path.replace( "R", "" ) ) <= MAX_RENDER_DEPTH:
 					urls_to_render.append( url )
-	logger.info( "Rendering " + str( len( urls_to_render ) ) + " URLs." )
 	open( WAYBACK_LOG, "wb" ).close()
-	now = datetime.now().strftime( "%Y%m%d%H%M%S" )
-	#TODO: Wayback should be running in proxy mode.
-	for url in urls_to_render:
-		try:
-			ignore = subprocess32.check_output( [ PHANTOMJS, NETSNIFF, WAYBACK + now + "/" + url ], timeout=20 )
-		except ( subprocess32.CalledProcessError, subprocess32.TimeoutExpired ):
-			pass
+	phantomjs_render( urls_to_render )
 	force = []
 	with open( WAYBACK_LOG, "rb" ) as l:
 		for line in l:
 			if "F+" in line:
 				force.append( re.sub( "^.*INFO: ", "", line ) )
 	if len( force ) > 0:
+		timestamp = datetime.now().strftime( "%Y%m%d%H%M%S" )
 		#Write the file 'one level up' to avoid conflicts with Heritrix reads.
-		action = open( JOB_ROOT + job + "/" + now + ".force", "wb" )
+		action = open( JOB_ROOT + job + "/" + timestamp + ".force", "wb" )
 		seen = []
 		for line in force:
 			url = line.split()[ 1 ]
@@ -318,7 +353,7 @@ def checkCompleteness( job, launchid ):
 				action.write( line + "\n" )
 				seen.append( url )
 		action.close()
-		shutil.move( JOB_ROOT + job + "/" + now + ".force", JOB_ROOT + job + "/action/" + now + ".force" )
+		shutil.move( JOB_ROOT + job + "/" + timestamp + ".force", JOB_ROOT + job + "/action/" + timestamp + ".force" )
 		logger.info( "Found " + str( len( seen ) ) + " distinct new URLs." )
 		# Pause briefly to allow file to be picked up.
 		time.sleep( 35 )
@@ -341,7 +376,7 @@ def humanReadable( bytes, precision=1 ):
 			break
 	return "%.*f %s" % ( precision, bytes / factor, suffix )
 
-def logStats( logs ):
+def log_stats( logs ):
 	response_codes = []
 	data_size = 0
 	host_regex = re.compile( "https?://([^/]+)/.*$" )
@@ -361,37 +396,71 @@ def logStats( logs ):
 					host_data[ "response_codes" ].append( fields[ 1 ] )
 					if fields[ 2 ] != "-":
 						host_data[ "data_size" ] += int( fields[ 2 ] )
+	all_hosts_data = OrderedDict( sorted( all_hosts_data.iteritems(), key=operator.itemgetter( 1 ), reverse=True ) )
 	for host, data in all_hosts_data.iteritems():
 		data[ "response_codes" ] = Counter( data[ "response_codes" ] )
 		data[ "data_size" ] = humanReadable( data[ "data_size" ] )
 	return all_hosts_data
 
-def checkUkwa( job, launchid ):
+def check_ukwa( job, launchid ):
+	global api
 	frequency = job.split( "-" )[ 0 ]
 	try:
 		xml = callAct( URL_ROOT + frequency )
 		xml = xml.read()
+		dom = etree.fromstring( xml )
 	except urllib2.URLError, e:
 		logger.error( "Cannot read ACT! " + str( e ) + " [" + job + "/" + launchid + "]" )
 		return
 	except httplib.IncompleteRead, i:
 		logger.error( "IncompleteRead: " + job + "/" + launchid )
 		return
+	# Find WCT IDs and corresponding URLs
+	wct_data = []
+	seeds = api.seeds( job )
 	for node in dom.findall( "node" ):
 		wct_id = node.find( "wctId" ).text
-		if wct_id is not None:
-			crawl_logs = glob.glob( LOG_ROOT + "/" + job + "/" + launchid + "/crawl.log*" )
-			for crawl_log in crawl_logs:		
-				( data_size, response_codes ) = log_stats( crawl_log )
+		urls = node.find( "urls" ).text
+		aid = node.find( "actLink" ).text.split( "/" )[ -1 ]
+		for url in urls.split():
+			if url in seeds:
+				if wct_id is not None:
+					wct_data.append( ( wct_id, aid, urls ) )
+	# Generate stats. from the logs
+	crawl_logs = glob.glob( LOG_ROOT + "/" + job + "/" + launchid + "/crawl.log*" )
+	stats = log_stats( crawl_logs )
+	js = json.dumps( stats, indent=8, separators=( ",", ":" ) )
+	return ( wct_data, js )
+
+def add_act_instance( target, timestamp, data, wct_data ):
+	wct_id, act_id, urls = wct_data
+	a_act = act.ACT()
+	body = []
+	content = {}
+	content[ "value" ] = "WCT ID: %s\nSeeds: %s\n\n<pre>%s</pre>" % ( wct_id, urls, data )
+	content[ "format" ] = "full_html"
+	body.append( content )
+	instance = {}
+	instance[ "type" ]  = "instance"
+	instance[ "body" ] = body
+	instance[ "field_timestamp" ] = timestamp
+	instance[ "field_target" ] = target
+	update = json.dumps( instance, sort_keys=False )
+	logger.info( update )
+	r = a_act.send_data( target, update )
+	return r
 
 if __name__ == "__main__":
-# Check for scheduled jobs.
+	# Check for scheduled jobs.
 	started_jobs = check_frequencies()
-# Check for EMPTY jobs and render for completeness.
+	#TODO 0: At this point, started_jobs should just be EMPTY.
+	#      Once the below has run, submitjob().
+	started_job_names = tuple( x[ 0 ] for x in started_jobs )
+	# Check for EMPTY jobs and render for completeness.
 	for port in set( heritrix_ports.values() ):
 		api = heritrix.API( host="https://opera.bl.uk:" + port + "/engine", user="admin", passwd="bl_uk", verbose=False, verify=False )
 		for emptyJob, launchid in jobsByStatus( "EMPTY" ):
-			if emptyJob in started_jobs:
+			if emptyJob in started_job_names:
 				# Don't kill jobs we've just started.
 				continue
 			if emptyJob.startswith( "latest" ):
@@ -404,4 +473,14 @@ if __name__ == "__main__":
 			waitfor( emptyJob, "FINISHED" )
 			api.teardown( emptyJob )
 			waitfor( emptyJob, "" )
+			# Check for UKWA Instances.
+			wct_data, stats = check_ukwa( emptyJob, launchid )
+			for datum in wct_data:
+				wct_id, act_id, urls = datum
+				logger.info( "WCT ID: %s" % wct_id )
+				logger.info( "ACT ID: %s" % act_id )
+				logger.info( "\tURLs: %s" % urls )
+				logger.info( "Adding Instance: %s" % act_id )
+				add_act_instance( act_id, launchid, stats, datum )
+			logger.info( stats )
 
