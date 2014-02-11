0b82cf2637d68e270eadb673e86e71daf31c2e98
Roger G. Coram <roger.coram@bl.uk>
2014-02-11 10:37:17 +0000

    Added install script for sanity's sake.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install "$(dirname "$0")/" --install-option="--prefix=/opt/sipd"
+
+[[ -d /var/run/sipd ]] || mkdir /var/run/sipd
+chown heritrix:heritrix /var/run/sipd
+chown -R heritrix:heritrix /opt/sipd/
+find /opt/sipd -name sipd.py -exec chmod +x '{}' \;
+
