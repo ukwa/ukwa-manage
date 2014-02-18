b5d935c1a0e6aec323e8c8a717538798fddd256b
Roger G. Coram <roger.coram@bl.uk>
2014-02-18 14:12:06 +0000

    Added size limit.

--- warcwriterpool/warcwriterpool.py
+++ warcwriterpool/warcwriterpool.py
@@ -1,32 +1,53 @@
 #!/usr/bin/env python
 """Writes records to a configurable number of WARC files."""
 
+import os
+import logging
 import itertools
 from datetime import datetime
 from hanzo.warctools import WarcRecord
 from hanzo.warctools.warc import warc_datetime_str
 
-class WarcWriterPool:
-	def __init__( self, num_warcs=1, gzip=True, prefix="BL", output_dir="." ):
-		self.gzip = gzip
-		self.current = 0
-		self.warc_files = []
-		self.output_dir = output_dir
+LOGGING_FORMAT="[%(asctime)s] %(levelname)s: %(message)s"
+logging.basicConfig( format=LOGGING_FORMAT, level=logging.DEBUG )
+logger = logging.getLogger( "warcwriterpool" )
 
-		self.warcs = []
+class WarcWriterPool:
+	def __init__( self, pool_size=1, gzip=True, prefix="BL", output_dir=".", max_size=1073741824 ):
+		self.gzip = gzip
+		self.prefix = prefix
+		self.output_dir = output_dir
+		self.max_size = max_size
+		self.warcs = {}
+
 		if gzip:
-			suffix = ".gz"
+			self.suffix = ".gz"
 		else:
-			suffix = ""
-		for n in range( num_warcs ):
-			self.warcs.append( open( "%s/%s-%s-%s.warc%s" % ( self.output_dir, prefix, datetime.now().strftime("%Y%m%d%H%M%S"), n, suffix ), "wb" ) )
-		self.pool = itertools.cycle( self.warcs )
+			self.suffix = ""
+		logger.debug( "Pooling %i WARCs." % pool_size )
+		self.add_warcs( pool_size )
+
+	def add_warcs( self, number ):
+		for n in range( number ):
+			name = "%s/%s-%s-%s.warc%s" % ( self.output_dir, self.prefix, datetime.now().strftime("%Y%m%d%H%M%S"), len( self.warcs.keys() ), self.suffix )
+			fh = open( name, "wb" )
+			self.warcs[ name ]  = fh
+			logger.debug( "Added %s" % name )
+		self.pool = itertools.cycle( self.warcs.iteritems() )
+
+	def check_warc_size( self, path ):
+		stat = os.stat( path )
+		if stat.st_size >= self.max_size:
+			logger.debug( "Size limit exceeded for %s" % path )
+			del self.warcs[ path ]
+			self.add_warcs( 1 )
 
 	def write_record( self, headers, mime, data ):
 		record = WarcRecord( headers=headers, content=( mime, data ) ) 
-		warc = self.pool.next()
-		record.write_to( warc, gzip=self.gzip )
-		warc.flush()
+		name, fh = self.pool.next()
+		record.write_to( fh, gzip=self.gzip )
+		fh.flush()
+		self.check_warc_size( name )
 
 	def cleanup( self ):
 		for w in self.warcs:
