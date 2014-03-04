a3cd3f1bdc1af8e966db2ea6e4ad3f0a46e7da2d
Roger G. Coram <roger.coram@bl.uk>
2014-03-04 17:53:22 +0000

    Updated to live queue name.

--- harchiverd/settings.py
+++ harchiverd/settings.py
@@ -1,6 +1,6 @@
 HAR_QUEUE_HOST="localhost"
-HAR_QUEUE_NAME="phantomjs-test"
-HAR_QUEUE_KEY="phantomjs-test"
+HAR_QUEUE_NAME="phantomjs"
+HAR_QUEUE_KEY="phantomjs"
 PID_FILE="/var/run/harchiverd/harchiverd.pid"
 LOG_FILE="/var/log/harchiverd/harchiverd.log"
 OUTPUT_DIRECTORY="/heritrix/output/images"
