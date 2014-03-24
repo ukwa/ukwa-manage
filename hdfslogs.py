37ae24240f43f3e4c5b24ea937e66dc12dab39b3
Roger G. Coram <roger.coram@bl.uk>
2014-03-24 10:11:58 +0000

    Repackaged.

---
+++ hdfslogs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Intended to keep local log files synced with HDFS.
+"""
+
+import os
+import glob
+import logging
+import webhdfs
+from hdfslogs import settings
+from socket import gethostname
+
+LOGGING_FORMAT="[%(asctime)s] %(levelname)s: %(message)s"
+logging.basicConfig( format=LOGGING_FORMAT, level=logging.INFO )
+logger = logging.getLogger( "hdfssync" )
+logging.root.setLevel( logging.INFO )
+logging.getLogger( "requests" ).setLevel( logging.WARNING )
+
+if __name__ == "__main__":
+	w = webhdfs.API( prefix="http://%s:14000/webhdfs/v1" % settings.hdfshost, user=settings.hdfsloguser )
+	for path in settings.logdirectories.split( "," ):
+		if not path.startswith( "/" ):
+			logger.warning( "Relative path found; ignoring: %s" % path )
+			continue
+		if not os.path.exists( path ):
+			logger.warning( "Trying to sync. non-existant path: %s" % path )
+			continue
+		if not path.endswith( "/" ):
+			path = "%s/" % path
+		hdfs_prefix = "%s/%s" % ( settings.hdfslogroot, gethostname() )
+		for log in glob.glob( "%s*" % path ):
+			local_size = os.stat( log ).st_size
+			if local_size == 0:
+				continue
+			hdfs_log = "%s%s" % ( hdfs_prefix, log )
+			if w.exists( hdfs_log ):
+				hdfs_size = int( w.list( hdfs_log )[ "FileStatuses" ][ "FileStatus" ][ 0 ][ "length" ] )
+				if hdfs_size != local_size:
+					logger.info( "Removing %s" % hdfs_log )
+					w.delete( hdfs_log )
+					if( w.exists( hdfs_log ) ):
+						logger.error( "Problem deleting %s" % hdfs_log )
+						continue
+				else:
+					continue
+			logger.info( "Creating %s" % hdfs_log )
+			r = w.create( hdfs_log, file=log )
+			if not r.ok or not w.exists( hdfs_log ):
+				logger.error( "Problem creating %s" % hdfs_log )
---
+++ hdfssync/settings.py
@@ -0,0 +1,4 @@
+hdfshost="194.66.232.90"
+hdfsloguser="tomcat"
+hdfslogroot="/logs"
+logdirectories="/opt/tomcat/logs"
