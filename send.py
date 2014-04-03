af66290127abbbf5b77117a926a53de01a736c7e
Roger G. Coram <roger.coram@bl.uk>
2014-04-03 11:11:18 +0100

    Added MQ transaction check.

--- send.py
+++ send.py
@@ -10,6 +10,7 @@ logging.basicConfig()
 connection = pika.BlockingConnection( pika.ConnectionParameters( settings.HAR_QUEUE_HOST ) )
 channel = connection.channel()
 channel.queue_declare( queue=settings.HAR_QUEUE_NAME, durable=True )
+channel.tx_select()
 channel.basic_publish( exchange="",
 	routing_key=settings.HAR_QUEUE_KEY,
 	properties=pika.BasicProperties(
@@ -17,5 +18,6 @@ channel.basic_publish( exchange="",
 	),
 	body=sys.argv[ 1 ]
 )
+channel.tx_commit()
 connection.close()
 
