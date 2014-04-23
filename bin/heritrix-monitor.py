d0b2411ecaf0cd43076258a75b1aeeffac2887bc
Roger G. Coram <roger.coram@bl.uk>
2014-04-23 12:02:49 +0100

    Reformatted message.

--- bin/heritrix-monitor.py
+++ bin/heritrix-monitor.py
@@ -11,16 +11,18 @@ services = { ( "opera.bl.uk", "8443" ), ( "opera.bl.uk", "8444" ), ( "opera.bl.u
 logging.basicConfig( format=LOGGING_FORMAT, level=logging.DEBUG )
 logger = logging.getLogger( "heritrix-monitor" )
 
-def send_message( port ):
-	message = "Could not connect to Heritrix on port " + port
-	t = Twitter( auth=OAuth( "16067043-PDJQCHs8JvjFbE0R98KBS8mOmgjboiDGecX8DzIwY", "PRb6uzccTe08mjOP565mCR3pZDxRxDyCSqCWfOhjg", "QQLmIMllFWxZKi0rdmPuSg", "Q6yGMQWBAlxgai3B45G9C8Na9i5k3bQ8hZ9j7JorIs" ) )
+def send_message( message ):
+	t = Twitter( auth=OAuth( "16067043-PDJQCHs8JvjFbE0R98KBS8mOmgjboiDGecX8DzIwY", "PRb6uzccTe08mjOP565mCR3pZDxRxDyCSqCWfOhjg", "Q2EiadOZrHp7cCk9ITCkGA", "2YtbjCuKwf8rnrTWV8Y0tomi30tEevcrTixi8jn8QMM" ) )
 	t.direct_messages.new( user="PsypherPunk", text=message )
 	logger.error( message )
 
+failures = []
 for host, port in services:
 	api = heritrix.API( host="https://%s:%s/engine" % ( host, port ), user="admin", passwd="bl_uk", verbose=False, verify=False )
 	try:
 		api.rescan()
 	except Exception:
-		send_message( port )
-	
+		failures.append( "%s@%s" % ( host, port ) )
+if len( failures ) > 0:
+	send_message( "Could not connect to Heritrix: %s." % ", ".join( failures ) )
+
