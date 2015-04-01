20dde5336178c4a6f4a81d78aa81b99552c6bf88
Roger G. Coram <roger.coram@bl.uk>
2015-04-01 11:56:45 +0100

    www.gov.uk validation.

--- w3act/watched/validation.py
+++ w3act/watched/validation.py
@@ -2,22 +2,34 @@
 Specific validation steps for Watched Targets.
 """
 
+import sys
 import logging
 import requests
 from lxml import html
+from urlparse import urlparse, urljoin
 
 logger = logging.getLogger("w3act")
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.DEBUG)
 logging.getLogger("requests").setLevel(logging.WARNING)
 requests.packages.urllib3.disable_warnings()
 
-def www_gov_uk(target, doc):
-    """The 'organisation-link' text specifies the Gov. Dept."""
+
+def validate(seed, doc):
+    key = urlparse(doc["landing_page_url"]).netloc.replace(".", "_")
+    try:
+        validator = getattr(sys.modules[__name__], key)
+        return validator(target, doc)
+    except AttributeError:
+        return True
+
+
+def www_gov_uk(seed, doc):
+    """Dept. links appear in a <aside class="meta"/> tag."""
     try:
         r = requests.get(doc["landing_page_url"])
         h = html.fromstring(r.content)
-        dept = h.xpath("//a[@class='organisation-link']")[0].text
-        return (dept in target["title"])
+        hrefs = [urljoin(doc["landing_page_url"], href) for href in h.xpath("//aside[contains(@class, 'meta')]//a/@href")]
+        return seed in hrefs
     except:
         logger.error("www_gov_uk: %s" % sys.exc_info()[0])
         return False
