9c5ba68e4b255066442ca19fef65a1873b4adbb0
Roger G. Coram <roger.coram@bl.uk>
2015-04-02 14:11:58 +0100

    Changes for frequency-based job handling.

--- w3act/settings.py
+++ w3act/settings.py
@@ -10,12 +10,13 @@ JOB_QUEUE_NAME="w3actqueue"
 
 W3ACT_BASE="https://www.webarchive.org.uk/act-ddhapt"
 W3ACT_LOGIN="%s/login" % W3ACT_BASE
-W3ACT_EMAIL="wa-sysadm@bl.uk"
-W3ACT_PASSWORD="6d1598bb-7b12-45ba-b266-275aa468bc64"
+W3ACT_EMAIL="roger.coram@bl.uk"
+W3ACT_PASSWORD="g07*$ECAVDU8QoU"
 W3ACT_EXPORT_BASE="%s/targets/export" % W3ACT_BASE
 W3ACT_LD_EXPORT="%s/ld" % W3ACT_EXPORT_BASE
 W3ACT_JOB_FIELD="url"
 
+HERITRIX_HOST="opera.bl.uk"
 HERITRIX_ROOT="/opt/heritrix"
 HERITRIX_JOBS="%s/jobs" % HERITRIX_ROOT
 HERITRIX_CONFIG_ROOT="/heritrix/git/heritrix_bl_configs"
@@ -26,3 +27,13 @@ HERITRIX_SURTS="%s/surts.txt" % HERITRIX_CONFIG_ROOT
 
 CLAMD_PORT="3310"
 
+JOB_RESTART_HOUR=12
+JOB_RESTART_WEEKDAY=1
+JOB_RESTART_DAY=1
+JOB_RESTART_MONTH=1
+W3ACT_FIELDS=["id", "title", "watched", "crawlStartDateText", "crawlEndDateText", "field_depth", "field_scope", "field_ignore_robots_txt"]
+
+FREQUENCIES = ["daily", "weekly", "monthly", "quarterly", "sixmonthly", "annual"]
+HERITRIX_PORTS = { "daily": "8443", "weekly": "8443", "monthly": "8443", "quarterly": "8443", "sixmonthly": "8443", "annual": "8443" }
+CLAMD_PORTS = { "daily": "3310", "weekly": "3310", "monthly": "3310", "quarterly": "3310", "sixmonthly": "3310", "annual": "3310" }
+MAX_RENDER_DEPTH = { "daily": 0, "weekly": 1, "monthly": 1, "quarterly": 1, "sixmonthly": 1, "annual": 1 }
