0f49bc78f34930eefc23befae809b1093a506235
Roger G. Coram <roger.coram@bl.uk>
2015-04-07 13:49:28 +0100

    Include licenced exports>

--- w3act/lib.py
+++ w3act/lib.py
@@ -6,6 +6,7 @@ import json
 import logging
 import requests
 import settings
+import traceback
 
 LOGGING_FORMAT="[%(asctime)s] %(levelname)s: %(message)s"
 logging.basicConfig(format=LOGGING_FORMAT, level=logging.DEBUG)
@@ -34,8 +35,12 @@ class ACT():
             js = json.loads(r.content)
         except:
             logger.warning(str(sys.exc_info()[0]))
+            logger.warning(str(traceback.format_exc()))
         return js
 
     def get_ld_export(self, frequency):
         return self._get_json("%s/ld/%s" % (settings.W3ACT_EXPORT_BASE, frequency))
+
+    def get_by_export(self, frequency):
+        return self._get_json("%s/by/%s" % (settings.W3ACT_EXPORT_BASE, frequency))
 
