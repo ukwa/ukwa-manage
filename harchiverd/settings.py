e7d82e62dc7c9e7b200d7e7d1bf33d9ed6676efb
Roger G. Coram <roger.coram@bl.uk>
2015-07-15 13:29:18 +0100

    Queue.

--- harchiverd/settings.py
+++ harchiverd/settings.py
@@ -7,5 +7,5 @@ AMQP_KEY="phantomjs"
 AMQP_URL="amqp://guest:guest@192.168.45.26:5672/%2f"
 AMQP_EXCHANGE="heritrix"
 AMQP_OUTLINK_QUEUE="heritrix-outlinks"
-AMQP_QUEUE="heritrix"
+AMQP_QUEUE="phantomjs"
 
