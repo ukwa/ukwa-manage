8bb46d2da78c2b687a776c35c915685b6e236cdc
Roger G. Coram <roger.coram@bl.uk>
2015-07-15 13:26:37 +0100

    Corrected server.

--- harchiverd/settings.py
+++ harchiverd/settings.py
@@ -1,7 +1,7 @@
 PID_FILE="/var/run/harchiverd/harchiverd.pid"
 LOG_FILE="/var/log/harchiverd/harchiverd.log"
 OUTPUT_DIRECTORY="/heritrix/output/images"
-WEBSERVICE="http://flock.bl.uk/webtools/domimage"
+WEBSERVICE="http://webrender.bl.uk/webtools/domimage"
 PROTOCOLS=["http", "https"]
 AMQP_KEY="phantomjs"
 AMQP_URL="amqp://guest:guest@194.66.232.91:5672/%2f"
