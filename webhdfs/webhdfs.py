ff075e22a4f9a46211d79d9684f569acdb63141f
Roger G. Coram <roger.coram@bl.uk>
2015-07-22 16:07:32 +0100

    Fixed whitespace.

--- webhdfs/webhdfs.py
+++ webhdfs/webhdfs.py
@@ -10,39 +10,39 @@ from fnmatch import fnmatch
 from StringIO import StringIO
 
 LOGGING_FORMAT="[%(asctime)s] %(levelname)s: %(message)s"
-logging.basicConfig( format=LOGGING_FORMAT, level=logging.WARNING )
-logger = logging.getLogger( "webhdfs" )
+logging.basicConfig(format=LOGGING_FORMAT, level=logging.WARNING)
+logger = logging.getLogger("webhdfs")
 
 
 class API():
-    def __init__( self, prefix="http://localhost:14000/webhdfs/v1", verbose=False, user="hadoop" ):
+    def __init__(self, prefix="http://localhost:14000/webhdfs/v1", verbose=False, user="hadoop"):
         self.verbose = verbose
         self.prefix = prefix
         self.user = user
 
-    def _get( self, path="/", op="GETFILESTATUS", stream=False ):
-        url = "%s%s?user.name=%s&op=%s" % ( self.prefix, path, self.user, op )
-        r = requests.get( url, stream=stream )
+    def _get(self, path="/", op="GETFILESTATUS", stream=False):
+        url = "%s%s?user.name=%s&op=%s" % (self.prefix, path, self.user, op)
+        r = requests.get(url, stream=stream)
         return r
 
-    def _post( self, path, file=None, data=None, op="CREATE" ):
-        url = "%s%s?user.name=%s&op=%s&data=true" % ( self.prefix, path, self.user, op )
-        headers = { "content-type": "application/octet-stream" }
+    def _post(self, path, file=None, data=None, op="CREATE"):
+        url = "%s%s?user.name=%s&op=%s&data=true" % (self.prefix, path, self.user, op)
+        headers = {"content-type": "application/octet-stream"}
         if file is not None:
-            with open( file, "rb" ) as f:
-                r = requests.put( url, headers=headers, data=f )
+            with open(file, "rb") as f:
+                r = requests.put(url, headers=headers, data=f)
         else:
-            r = requests.put( url, headers=headers, data=data )
+            r = requests.put(url, headers=headers, data=data)
         return r
 
-    def _delete( self, path, recursive=False ):
-        url = "%s%s?user.name=%s&op=DELETE&recursive=%s" % ( self.prefix, path, self.user, str( recursive ).lower() )
-        r = requests.delete( url )
+    def _delete(self, path, recursive=False):
+        url = "%s%s?user.name=%s&op=DELETE&recursive=%s" % (self.prefix, path, self.user, str(recursive).lower())
+        r = requests.delete(url)
         return r
 
-    def list( self, path ):
-        r = self._get( path=path, op="LISTSTATUS" )
-        return json.loads( r.text )
+    def list(self, path):
+        r = self._get(path=path, op="LISTSTATUS")
+        return json.loads(r.text)
 
     def find(self, path, name="*"):
         if self.isdir(path):
@@ -57,51 +57,51 @@ class API():
         r = self._get(path=path, op="GETFILESTATUS")
         return json.loads(r.text)
 
-    def open( self, path ):
-        r = self._get( path=path, op="OPEN" )
+    def open(self, path):
+        r = self._get(path=path, op="OPEN")
         return r.content
 
-    def openstream( self, path ):
-        return self._get( path=path, op="OPEN", stream=True )
+    def openstream(self, path):
+        return self._get(path=path, op="OPEN", stream=True)
 
-    def exists( self, path ):
-        r = self._get( path=path )
-        j = json.loads( r.text )
-        return j.has_key( "FileStatus" )
+    def exists(self, path):
+        r = self._get(path=path)
+        j = json.loads(r.text)
+        return j.has_key("FileStatus")
 
-    def isdir( self, path ):
-        r = self._get( path=path )
-        j = json.loads( r.text )
-        return ( self.exists( path ) and j[ "FileStatus" ][ "type" ] == "DIRECTORY" )
+    def isdir(self, path):
+        r = self._get(path=path)
+        j = json.loads(r.text)
+        return (self.exists(path) and j["FileStatus"]["type"] == "DIRECTORY")
 
-    def download( self, path, output=sys.stdout ):
+    def download(self, path, output=sys.stdout):
         """Copies a single file from HDFS to a local file."""
-        r = self.openstream( path )
-        for chunk in r.iter_content( chunk_size=4096 ):
+        r = self.openstream(path)
+        for chunk in r.iter_content(chunk_size=4096):
             if chunk:
-                o.write( chunk )
+                o.write(chunk)
                 o.flush()
 
-    def create( self, path, file=None, data=None ):
-        if ( file is None and data is None ) or ( file is not None and data is not None ):
-            logger.warning( "Need either input file or data." )
+    def create(self, path, file=None, data=None):
+        if (file is None and data is None) or (file is not None and data is not None):
+            logger.warning("Need either input file or data.")
         else:
             if file is not None:
-                r = self._post( path, file=file )
+                r = self._post(path, file=file)
             else:
-                r = self._post( path, data=data )
+                r = self._post(path, data=data)
             return r
 
-    def delete( self, path, recursive=False ):
-        if not self.exists( path ):
-            logger.error( "Does not exist: %s" % path )
+    def delete(self, path, recursive=False):
+        if not self.exists(path):
+            logger.error("Does not exist: %s" % path)
         else:
-            r = self._delete( path, recursive=recursive )
-            return json.loads( r.text )
+            r = self._delete(path, recursive=recursive)
+            return json.loads(r.text)
 
-    def checksum( self, path ):
-        r = self._get( path=path, op="GETFILECHECKSUM" )
-        return json.loads( r.text )
+    def checksum(self, path):
+        r = self._get(path=path, op="GETFILECHECKSUM")
+        return json.loads(r.text)
 
     def genblocks(self, path, chunk_size=4096, decompress=False):
         """Yields blocks from either a directory or a single file."""
@@ -123,7 +123,7 @@ class API():
                             yield chunk
                 r.close()
 
-    def getmerge( self, path, output=sys.stdout ):
+    def getmerge(self, path, output=sys.stdout):
         """Merges one or more HDFS files into a single, local file."""
         for chunk in self.genblocks(path):
             output.write(chunk)
