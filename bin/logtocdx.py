df7c0bb160419960b7491e70dc704925d1067be9
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-12-02 23:17:17 +0000

    Updated after more testing, skipping non-http/s and revisits.

--- bin/logtocdx.py
+++ bin/logtocdx.py
@@ -17,6 +17,10 @@ QUEUE_NAME = os.environ['QUEUE_NAME']
 LOG_FILE = os.environ['LOG_FILE']
 DUMMY = os.environ['DUMMY_RUN']
 
+# Should we skip duplicate records?
+# It seems OWB cope with them.
+skip_duplicates = True
+
 logger = logging.getLogger( "logtocdx" )
 handler = logging.FileHandler( LOG_FILE )
 formatter = logging.Formatter( "[%(asctime)s] %(levelname)s: %(message)s" )
@@ -33,6 +37,16 @@ logging.getLogger( "" ).addHandler( handler )
 # - 20151201225932 http://anjackson.net/projects/images/keeping-codes.png image/png 200 sha1:DDOWG5GHKEDGUCOCOXZCAPRUXPND7GOK - - -     593544 BL-20151201225813592-00001-37~157a2278f619~8443.warc.gz
 # - 20151202001114 http://anjackson.net/robots.txt unknown 302 sha1:3I42H3S6NNFQ2MSVX7XZKYAYSCX5QBYJ http://anjackson.net/ - - 773 BL-20151202001107925-00001-41~157a2278f619~8443.warc.gz
 
+# Example de-duplicated CDX line from pywb cdx-indexer:
+#
+# net,anjackson)/assets/js/ie8-responsive-file-warning.js 20151202230549 http://anjackson.net/assets/js/ie8-responsive-file-warning.js text/html 404 HJ66ECSQVYNX22SEAFF7QAF4AZYKN2BD - - 2755 2945445 BL-20151202230405810-00000-38~101e6c786d7f~8443.warc.gz
+# net,anjackson)/assets/js/ie8-responsive-file-warning.js 20151202230632 http://anjackson.net/assets/js/ie8-responsive-file-warning.js warc/revisit - HJ66ECSQVYNX22SEAFF7QAF4AZYKN2BD - - 548 3604638 BL-20151202230405810-00000-38~101e6c786d7f~8443.warc.gz
+
+
+#[2015-12-02 22:35:45,851] ERROR: Failed with 400 Bad Request
+#java.lang.NumberFormatException: For input string: "None"
+#At line: - 20151202223545 dns:447119634 text/dns 1001 None - - - None None
+
 def callback( ch, method, properties, body ):
 	"""Passed a crawl log entry, it turns it into a CDX line and posts it to the index."""
 	try:
@@ -40,27 +54,34 @@ def callback( ch, method, properties, body ):
 		cl = json.loads(body)
 		url = cl["url"]
 		# Skip non http(s) records (?)
-		#if( not url[:4] == "http"):
-		#	ch.basic_ack(delivery_tag = method.delivery_tag)
+		if( not url[:4] == "http"):
+			ch.basic_ack(delivery_tag = method.delivery_tag)
+			return
 		redirect = "-"
 		status_code = cl["status_code"]
 		# Don't index negative status codes here:
 		if( status_code <= 0 ):
-			logger.info("Ignoring -ve status_code log entry: %s" % body)
-			ch.basic_ack(delivery_tag = method.delivery_tag)
-			return
-		# Don't index revisit records as OW can't handle them (?)
-		if "duplicate:digest" in cl["annotations"]:
-			logger.info("Skipping de-duplicated resource: %s" % body)
+			logger.info("Ignoring <=0 status_code log entry: %s" % body)
 			ch.basic_ack(delivery_tag = method.delivery_tag)
 			return
 		# Record via for redirects:
 		if( status_code/100 == 3 ):
 			redirect = cl["via"]
+		# Don't index revisit records as OW can't handle them (?)
+		mimetype = cl["mimetype"]
+		if "duplicate:digest" in cl["annotations"]:
+			if skip_duplicates:
+ 				logger.info("Skipping de-duplicated resource: %s" % body)
+				ch.basic_ack(delivery_tag = method.delivery_tag)
+				return
+			else:
+				mimetype = "warc/revisit"
+				status_code = "-"
+		# Build CDX line:
 		cdx_11 = "- %s %s %s %s %s %s - - %s %s\n" % ( 
 			cl["start_time_plus_duration"][:14],
 			url,
-			cl["mimetype"],
+			mimetype,
 			status_code,
 			cl["content_digest"],
 			redirect,
@@ -90,7 +111,7 @@ if __name__ == "__main__":
 		channel.exchange_declare(exchange="heritrix", durable=True)
 		channel.queue_declare( queue=QUEUE_NAME, durable=True )
 		channel.queue_bind(queue=QUEUE_NAME, exchange="heritrix", routing_key="crawl-log-feed")
-		channel.basic_qos(prefetch_count=1)
+		channel.basic_qos(prefetch_count=10)
 		channel.basic_consume( callback, queue=QUEUE_NAME, no_ack=False )
 		channel.start_consuming()
 	except Exception as e:
