225497b9f58d39fd7a6859cb9de7cf986306fc0f
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-01-16 20:22:17 +0000

    Renaming for frequent crawl.

--- agents/fc-drover.py
+++ agents/fc-drover.py
@@ -3,9 +3,26 @@
 """Drives the crawls. Downloads the crawl feeds and initiates crawls as appropriate by dropping messages on the right queue."""
 
 import os
+import logging
 import pika
 import argparse
 from shared.w3act import w3act
+
+# Set up a logging handler:
+handler = logging.StreamHandler()
+#handler = logging.StreamHandler(sys.stdout) # To use stdout rather than the default stderr
+formatter = logging.Formatter( "[%(asctime)s] %(levelname)s %(filename)s.%(funcName)s: %(message)s" )
+handler.setFormatter( formatter ) 
+
+# Attach to root logger
+logging.root.addHandler( handler )
+
+# Set default logging output for all modules.
+logging.root.setLevel( logging.WARNING )
+
+# Set logging for this module and keep the reference handy:
+logger = logging.getLogger( __name__ )
+logger.setLevel( logging.INFO )
 
 
 if __name__ == "__main__":
@@ -20,11 +37,23 @@ if __name__ == "__main__":
 	parser.add_argument('queue', metavar='queue', help="Name of queue to view messages from.")
 	args = parser.parse_args()
 	
+	# Get all the frequently-crawled items
 	act = w3act("http://localhost:9000/act/","wa-sysadm@bl.uk","sysAdmin")
-	print(act.get_ld_export('frequent'));
+	logger.info(act.get_ld_export('frequent'));
 	
+	# Determine if any are due to start in the current hour
+	
+	# Push a 'seed' message onto the rendering queue:
 	parameters = pika.URLParameters(args.amqp_url)
 	connection = pika.BlockingConnection( parameters )
 	channel = connection.channel()
 	channel.queue_bind(queue=args.queue, exchange=args.exchange)
+	
+	# Note that the other aspects, like depth etc, and setup periodically via fc-shaper.
+	
+	# Separate process bundles up per checkpoint (gather.py)
+	
+	# Separate process sends Documents to a queue (in H3) and sends the queue to W3ACT (mule.py)
+	# muster.py, yoke.py, shear.py, rouseabout, riggwelter (upside down sheep), 
+	# lanolin (grease), cull.py, heft (land), flock, fold, dip, bellwether (flock lead)
 
