767b8a4dd1724d08c6ee40c2b2fa9cb607a9b39e
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-05-12 16:30:16 +0100

    Removed unnecessary code.

--- lib/sip/ids.py
+++ lib/sip/ids.py
@@ -17,45 +17,6 @@ NS={"mets": "http://www.loc.gov/METS/", "premis": "info:lc/xmlns/premis-v2"}
 XLINK="{http://www.w3.org/1999/xlink}"
 WEBHDFS="http://194.66.232.90:14000/webhdfs/v1"
 
-LOGGING_FORMAT="[%(asctime)s] %(levelname)s: %(message)s"
-logging.basicConfig(format=LOGGING_FORMAT)
-logger = logging.getLogger("sipverify.lib")
-logger.setLevel(logging.INFO)
-logging.getLogger("pika").setLevel(logging.ERROR)
-
-def get_message(host, queue):
-	"""Pulls a single message from a queue."""
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host))
-	channel = connection.channel()
-	method_frame, header_frame, body = channel.basic_get(queue)
-	if method_frame:
-		logger.debug("%s, %s, %s" % (method_frame, header_frame, body))
-		channel.basic_ack(method_frame.delivery_tag)
-		connection.close()
-		return body
-	connection.close()
-
-def isvalid(message):
-	"""Verifies that a message is valid. i.e. it's similar to: 'daily-0400/20140207041736'"""
-        r = re.compile("^[^/]+/[0-9]+")
-        return r.match(message)
-
-def put_message(host, queue, message):
-	"""Puts a message back on the appropriate queue."""
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host))
-	channel = connection.channel()
-	channel.queue_declare(queue=queue, durable=True)
-	channel.tx_select()
-	channel.basic_publish(exchange="",
-		routing_key=queue,
-		properties=pika.BasicProperties(
-			delivery_mode=2,
-		),
-		body=message
-	)
-	channel.tx_commit()
-	connection.close()
-
 def get_identifiers(sip):
 	"""Parses the SIP in HDFS and retrieves ARKs."""
 	w = webhdfs.API(prefix=WEBHDFS)
