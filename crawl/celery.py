5de7733604c567d20dc841d0069a190d9b3b3c35
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-07-26 23:45:20 +0100

    Explicit queues for distinct processes.

--- crawl/celery.py
+++ crawl/celery.py
@@ -47,36 +47,64 @@ app.conf.update(
             "exchange": "default",
             "binding_key": "default"
         },
-        # "RC1-restart-job": {
-        #     "exchange": "default",
-        #     "binding_key": "crawl.tasks.restart_job",
-        # },
-        # "RC2-validate-job": {
-        #     "exchange": "default",
-        #     "binding_key": "crawl.tasks.validate_job",
-        # }
-        "RC10-uri-to-index": {
+        'PULSE-01-stop-start-job': {
+            "exchange": "default",
+            "binding_key": "crawl.tasks.stop_start_job",
+        },
+        "PULSE-02-assemble-job-output": {
+            "exchange": "default",
+            "binding_key": "crawl.tasks.assemble_job_output",
+        },
+        "PULSE-03-build-sip": {
+            "exchange": "default",
+            "binding_key": "crawl.tasks.build_sip",
+        },
+        "PULSE-04-submit-sip": {
+            "exchange": "default",
+            "binding_key": "crawl.tasks.submit_sip",
+        },
+        "PULSE-05-verify-sip": {
+            "exchange": "default",
+            "binding_key": "crawl.tasks.verify_sip",
+        },
+        "PULSE-10-uri-to-index": {
             "exchange": "default",
             "binding_key": "crawl.tasks.uri_to_index",
         },
-        "RC11-uri-to-doc": {
+        "PULSE-11-uri-to-doc": {
             "exchange": "default",
             "binding_key": "crawl.tasks.uri_of_doc",
+        },
+        "STATUS-01-crawl-state-updates": {
+            "exchange": "default",
+            "binding_key": "crawl.status.update_job_status",
         }
     },
     # Mapping from tasks to queues:
     CELERY_ROUTES = {
-        # 'crawl.tasks.restart_job': {
-        #     'queue' : 'RC1-restart-job',
-        # },
-        # 'crawl.tasks.validate_job': {
-        #     'queue': 'RC2-validate-job',
-        # },
+        'crawl.tasks.stop_start_job': {
+             'queue' : 'PULSE-01-stop-start-job',
+        },
+        'crawl.tasks.assemble_job_output': {
+             'queue': 'PULSE-02-assemble-job-output',
+        },
+        'crawl.tasks.build_sip': {
+             'queue': 'PULSE-03-build-sip',
+        },
+        'crawl.tasks.submit_sip': {
+             'queue': 'PULSE-04-submit-sip',
+        },
+        'crawl.tasks.verify_sip': {
+             'queue': 'PULSE-05-verify-sip',
+        },
         'crawl.tasks.uri_to_index': {
-            'queue': 'RC10-uri-to-index',
+            'queue': 'PULSE-10-uri-to-index',
         },
         'crawl.tasks.uri_of_doc': {
-            'queue': 'RC11-uri-to-doc',
+            'queue': 'PULSE-11-uri-to-doc',
+        },
+        'crawl.status.update_job_status': {
+            'queue': 'STATUS-01-crawl-state-updates',
         },
     },
     # Default routing:
