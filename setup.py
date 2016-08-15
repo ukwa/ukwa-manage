b8147bccfb646f4b07d44af9563c2f57dae3e904
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-08-15 12:02:11 +0100

    Ensure all files are included.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     name='shepherd',
     version='1.0.0',
     packages=['crawl', 'crawl.h3', 'crawl.hdfs', 'crawl.job', 'crawl.profiles',
- 'crawl.sip', 'crawl.w3act', 'crawl.cdx', 'crawl.harchiverd', 'crawl.dex'],
+ 'crawl.sip', 'crawl.w3act', 'crawl.cdx', 'crawl.harchiverd', 'crawl.dex', 'crawl.ui', 'crawl.ui.templates'],
     install_requires=requirements,
     package_data={
         # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.xml', '*.cfg', '*.cxml'],
+        '': ['*.txt', '*.xml', '*.cfg', '*.cxml', '*.json', '*.html'],
     },
 
     license='Apache 2.0',
