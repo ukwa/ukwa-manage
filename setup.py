395d11448749517fe3666260edcc713830ff28e8
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-08-15 16:14:37 +0100

    Local CSS setup right.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     name='shepherd',
     version='1.0.0',
     packages=['crawl', 'crawl.h3', 'crawl.hdfs', 'crawl.job', 'crawl.profiles',
- 'crawl.sip', 'crawl.w3act', 'crawl.cdx', 'crawl.harchiverd', 'crawl.dex', 'crawl.ui', 'crawl.ui.templates'],
+ 'crawl.sip', 'crawl.w3act', 'crawl.cdx', 'crawl.harchiverd', 'crawl.dex', 'crawl.ui', 'crawl.ui.templates', 'crawl.ui.static'],
     install_requires=requirements,
     package_data={
         # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.xml', '*.cfg', '*.cxml', '*.json', '*.html'],
+        '': ['*.txt', '*.xml', '*.cfg', '*.cxml', '*.json', '*.html', '*.css', '*.otf', '*.eot', '*.svg', '*.ttf', '*.woff*' ],
     },
 
     license='Apache 2.0',
