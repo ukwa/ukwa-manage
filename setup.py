f00f7a0448bc7ade8cfaad4cdc615b56849029e2
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-08-15 16:43:17 +0100

    Should install everything now.

--- setup.py
+++ setup.py
@@ -6,13 +6,8 @@ with open('requirements.txt') as f:
 setup(
     name='shepherd',
     version='1.0.0',
-    packages=['crawl', 'crawl.h3', 'crawl.hdfs', 'crawl.job', 'crawl.profiles',
- 'crawl.sip', 'crawl.w3act', 'crawl.cdx', 'crawl.harchiverd', 'crawl.dex', 'crawl.ui', 'crawl.ui.templates', 'crawl.ui.static'],
+    packages=find_packages(),
     install_requires=requirements,
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.xml', '*.cfg', '*.cxml', '*.json', '*.html', '*.css', '*.otf', '*.eot', '*.svg', '*.ttf', '*.woff*' ],
-    },
 
     license='Apache 2.0',
     long_description=open('README.md').read(),
