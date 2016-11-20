c9c9c7a65f0f6bc268eec37dfafd0acff3be30d5
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-11-20 21:08:38 +0000

    Moved older code into 'legacy' folder.

--- tasks/move_to_hdfs.py
+++ tasks/move_to_hdfs.py
@@ -9,6 +9,7 @@ import hashlib
 import threading
 import subprocess
 import luigi.contrib.hdfs
+import luigi.contrib.hadoop_jar
 from common import *
 from crawl_job_tasks import CheckJobStopped
 
@@ -327,8 +328,44 @@ class ScanForFilesToMove(ScanForLaunches):
         logger.info("Looking at moving files for %s %s" %(job, launch))
         yield MoveFilesForLaunch(job, launch, self.delete_local)
 
+
+DEFAULT_TERASORT_IN = '/tmp/terasort-in'
+DEFAULT_TERASORT_OUT = '/tmp/terasort-out'
+
+
+class TeraGen(luigi.contrib.hadoop_jar.HadoopJarJobTask):
+    """
+    Runs TeraGen, by default with 100KB of data (1000 records)
+    """
+
+    records = luigi.Parameter(default="1000",
+                              description="Number of records, each record is 100 Bytes")
+    terasort_in = luigi.Parameter(default=DEFAULT_TERASORT_IN,
+                                  description="directory to store terasort input into.")
+
+    def output(self):
+        """
+        Returns the target output for this task.
+        In this case, a successful execution of this task will create a file in HDFS.
+        :return: the target output for this task.
+        :rtype: object (:py:class:`~luigi.target.Target`)
+        """
+        return luigi.contrib.hdfs.HdfsTarget(self.terasort_in)
+
+    def jar(self):
+        return "./jars/hadoop-mapreduce-examples-2.7.0.jar"
+        #return "/usr/local/hadoop-2.7.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.0.jar"
+
+    def main(self):
+        return "teragen"
+
+    def args(self):
+        # First arg is 10B -- each record is 100bytes
+        return [self.records, self.output()]
+
 if __name__ == '__main__':
-    luigi.run(['scan.ScanForFilesToMove', '--date-interval', '2016-11-01-2016-11-10'])
+    luigi.run(['TeraGen'])
+#    luigi.run(['scan.ScanForFilesToMove', '--date-interval', '2016-11-01-2016-11-10'])
     #luigi.run(['file.ForceUploadFileToHDFS', '--path', '/Users/andy/Documents/workspace/pulse/testing/output/logs/daily/20161029192642/progress-statistics.log'])
 #    luigi.run(['file.ScanForFiles', '--date-interval', '2016-10-26-2016-10-30'])  # , '--local-scheduler'])
 #    luigi.run(['file.MoveToHdfs', '--path', '/Users/andy/Documents/workspace/pulse/python-shepherd/MANIFEST.in'])  # , '--local-scheduler'])
