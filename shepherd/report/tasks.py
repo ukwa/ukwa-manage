3a5d0b43ee8ec855959c88cd878997fda0fcb876
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-12-08 21:18:07 +0000

    Add output.

--- shepherd/report/tasks.py
+++ shepherd/report/tasks.py
@@ -28,7 +28,7 @@ class GenerateWarcStats(luigi.contrib.hadoop_jar.HadoopJarJobTask):
         return "uk.bl.wa.hadoop.mapreduce.warcstats.WARCStatsTool"
 
     def args(self):
-        return [self.input_file]
+        return [self.input_file, self.output().path]
 
 
 if __name__ == '__main__':
