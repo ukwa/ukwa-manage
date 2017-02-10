448a519feafcd889f43f7d6af1d835c1fee59512
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-02-10 09:27:22 +0000

    Shifting code around and moving to remote operation.

---
+++ tasks/heritrix/crawl_job_tasks.py
@@ -0,0 +1,166 @@
+import os
+import re
+import enum
+import json
+import luigi
+import datetime
+import glob
+import gzip
+import time
+from dateutil.parser import parse
+import zipfile
+import logging
+from crawl.w3act.w3act import w3act
+import crawl.h3.hapyx as hapyx
+from crawl.w3act.job import W3actJob
+from crawl.w3act.job import remove_action_files
+
+logger = logging.getLogger('luigi-interface')
+
+
+def get_hapy_for_job(job):
+    if h3().host == 'localhost':
+        host = "https://%s:%s" % (h3().host, h3().port)
+    else:
+        host = "https://%s-%s:%s" % (h3().host, job.name, h3().port)
+    return hapyx.HapyX(host, username=h3().username, password=h3().password)
+
+
+def mark_job_as(job, launch_id, mark):
+    record = jtarget(job, launch_id, mark)
+    with record.open('w') as f:
+        f.write('{} {}\n'.format(job.name, launch_id))
+
+
+class StopJobExternalTask(luigi.ExternalTask):
+    """
+    This task is used to mark jobs as stopped, but this is not something that can be forced automatically, see StopJob.
+    """
+    task_namespace = 'pulse'
+    job = luigi.EnumParameter(enum=Jobs)
+    launch_id = luigi.Parameter()
+
+    def output(self):
+        return jtarget(self.job, self.launch_id, 'stopped')
+
+
+class CheckJobStopped(luigi.Task):
+    """
+    Checks if given job/launch is currently running. Will not force the crawl to stop.
+    """
+    task_namespace = 'pulse'
+    job = luigi.EnumParameter(enum=Jobs)
+    launch_id = luigi.Parameter()
+
+    def output(self):
+        return jtarget(self.job, self.launch_id, 'stopped')
+
+    def run(self):
+        # Set up connection to H3:
+        h = get_hapy_for_job(self.job)
+
+        # Is that job running?
+        status = h.status(self.job.name)
+        if status != "":
+            # Check the launch ID is not current:
+            launch_id = h.get_launch_id(self.job.name)
+            if launch_id == self.launch_id:
+                # Declare that we are awaiting an external process to stop this job:
+                yield StopJobExternalTask(self.job, self.launch_id)
+
+        # Not running, so mark as stopped:
+        with self.output().open('w') as f:
+            f.write('{} {}\n'.format(self.job.name, self.launch_id))
+
+
+class StopJob(luigi.Task):
+    task_namespace = 'pulse'
+    job = luigi.EnumParameter(enum=Jobs)
+    date = luigi.DateParameter(default=datetime.date.today())
+
+    def complete(self):
+        # Set up connection to H3:
+        h = get_hapy_for_job(self.job)
+
+        # Is this job known?
+        if self.job.name in h.list_jobs():
+            status = h.status(self.job.name)
+            if status == "":
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def run(self):
+        # Set up connection to H3:
+        h = get_hapy_for_job(self.job)
+
+        logger.info("I'm stopping %s" % (self.job.name))
+
+        # Stop job if currently running:
+        if self.job.name in h.list_jobs() and h.status(self.job.name) != "":
+            """Stops a running job, cleans up the directory, initiates job assembly."""
+            launch_id = h.get_launch_id(self.job.name)
+            job = W3actJob.from_directory("%s/%s" % (h3().local_job_folder, self.job.name), heritrix=h)
+            job.stop()
+            remove_action_files(self.job.name, HERITRIX_JOBS=h3().local_job_folder)
+
+            # Record an output file that can be use as a Target by a different task:
+            mark_job_as(job, launch_id, 'stopped')
+        else:
+            logger.warning("No {} job to be stopped!".format(self.job.name))
+
+
+class StartJob(luigi.Task):
+    task_namespace = 'pulse'
+    job = luigi.EnumParameter(enum=Jobs)
+    date = luigi.DateMinuteParameter(default=datetime.datetime.today())
+    from_latest_checkpoint = luigi.BoolParameter(default=False)
+
+    def requires(self):
+        return [ StopJob(self.job), CrawlFeed(frequency=self.job.name), CrawlFeed(frequency='nevercrawl') ]
+
+    # Do no output anything, as we don't want anything to prevent restarts, or initiate downstream actions.
+    def output(self):
+        return luigi.LocalTarget('state/jobs/{}.started.{}'.format(self.job.name, self.date.isoformat()))
+
+    # Always allow re-starting:
+    #def complete(self):
+    #    return False
+
+    def run(self):
+        # Set up connection to H3:
+        h = get_hapy_for_job(self.job)
+
+        logger.info("Starting %s" % (self.job.name))
+        targets = json.load(self.input()[1].open('r'))
+        nevercrawl = json.load(self.input()[2].open('r'))
+        logger.debug("Found %s Targets in date range." % len(targets))
+        job = W3actJob(targets, self.job.name, heritrix=h, heritrix_job_dir=h3().local_job_folder, nevercrawl=nevercrawl)
+        status = h.status(self.job.name)
+        logger.info("Got current job status: %s" % status)
+
+        logger.info("Starting job %s (from checkpoint = %s)..." % (job.name, self.from_latest_checkpoint))
+        job.start(from_latest_checkpoint=self.from_latest_checkpoint)
+        launch_id = h.get_launch_id(self.job.name)
+
+        logger.info("Launched job %s/%s with %s seeds." % (job.name, launch_id, len(job.seeds)))
+        with self.output().open('w') as f:
+            f.write('{}\n'.format(launch_id))
+
+        # Record an output file that can be use as a Target by a different task.:
+        mark_job_as(job, launch_id, 'started')
+
+        return
+
+
+#@StartJob.event_handler(luigi.Event.SUCCESS)
+#def run_task_success(task):
+#    celebrate_success(task)
+
+
+if __name__ == '__main__':
+    #luigi.run(['pulse.StopJob', '--job', 'daily', '--local-scheduler'])
+    luigi.run(['pulse.StartJob', '--job', 'daily', '--local-scheduler'])
+    #luigi.run(['pulse.StartJob', '--job', 'daily', '--from-latest-checkpoint'])
--- tasks/remote/assembler.py
+++ tasks/remote/assembler.py
@@ -1,23 +1,45 @@
 import os
 import re
-import enum
 import json
 import luigi
+import luigi.contrib.ssh
+import luigi.contrib.hdfs
 import datetime
-import glob
 import gzip
-import time
+import string
+import hashlib
 from dateutil.parser import parse
 import zipfile
 import logging
-from slackclient import SlackClient
-from crawl.w3act.w3act import w3act
-import crawl.h3.hapyx as hapyx
-from crawl.w3act.job import W3actJob
-from crawl.w3act.job import remove_action_files
-from common import *
-from crawl_job_tasks import CheckJobStopped
-from move_to_hdfs import CalculateLocalHash, get_hdfs_target
+from tasks.heritrix.crawl_job_tasks import CheckJobStopped
+
+logger = logging.getLogger('luigi-interface')
+
+LUIGI_STATE_FOLDER = os.environ['LUIGI_STATE_FOLDER']
+HDFS_PREFIX = os.environ['HDFS_PREFIX']
+LOCAL_PREFIX = os.environ['LOCAL_PREFIX']
+LOCAL_JOB_FOLDER = os.environ.get('LOCAL_JOB_FOLDER','/heritrix/jobs')
+LOCAL_OUTPUT_FOLDER = os.environ.get('LOCAL_OUTPUT_FOLDER','/heritrix/output')
+LOCAL_WREN_FOLDER = os.environ.get('LOCAL_WREN_FOLDER','/heritrix/wren')
+#SSH_KWARGS = { 'user': 'heritrix', 'key_file': 'id_rsa' }
+
+
+def remote_glob(host, path):
+    rf = luigi.contrib.ssh.RemoteFileSystem(host)#, **SSH_KWARGS)
+    return rf.listdir(path)
+    #rc = luigi.contrib.ssh.RemoteContext(host=host)
+    #for item in rc.check_output('ls -1d %s' % path):
+    #    yield item
+
+
+def get_hdfs_target(path):
+    # Prefix the original path with the HDFS root folder, stripping any leading '/' so the path is considered relative
+    hdfs_path = os.path.join(HDFS_PREFIX, path.lstrip(os.path.sep))
+    return luigi.contrib.hdfs.HdfsTarget(hdfs_path)
+
+
+def target_name(state_class, job, launch_id, status):
+    return '{}-{}/{}/{}/{}.{}.{}.{}'.format(launch_id[:4],launch_id[4:6], job.name, launch_id, state_class, job.name, launch_id, status)
 
 
 def get_stage_suffix(stage):
@@ -32,12 +54,48 @@ def get_stage_suffix(stage):
         return ".%s" % stage
 
 
+class CalculateRemoteHash(luigi.Task):
+    task_namespace = 'file'
+    host = luigi.Parameter()
+    path = luigi.Parameter()
+
+    def output(self):
+        return self.hash_target(self.job, self.launch_id, "%s.local.sha512" % self.path)
+
+    def run(self):
+        logger.debug("file %s to hash" % (self.path))
+
+        t = luigi.contrib.ssh.RemoteTarget(self.path,self.host)#, **SSH_KWARGS)
+        with t.open('r') as reader:
+            file_hash = hashlib.sha512(reader.read()).hexdigest()
+
+        # test hash
+        CalculateRemoteHash.check_hash(self.path, file_hash)
+
+        with self.output().open('w') as f:
+            f.write(file_hash)
+
+    @staticmethod
+    def check_hash(path, file_hash):
+        logger.debug("Checking file %s hash %s" % (path, file_hash))
+        if len(file_hash) != 128:
+            raise Exception("%s hash not 128 character length [%s]" % (path, len(file_hash)))
+        if not all(c in string.hexdigits for c in file_hash):
+            raise Exception("%s hash not all hex [%s]" % (path, file_hash))
+
+    @staticmethod
+    def hash_target(job, launch_id, file):
+        return luigi.LocalTarget('{}/{}'.format(LUIGI_STATE_FOLDER, target_name('files/hash', job, launch_id,
+                                                                              os.path.basename(file))))
+
+
 class PackageLogs(luigi.Task):
     """
     For a given job launch and stage, package up the logs and config in a ZIP to go inside the main crawl SIP.
     """
     task_namespace = 'output'
-    job = luigi.EnumParameter(enum=Jobs)
+    host = luigi.Parameter()
+    job = luigi.Parameter()
     launch_id = luigi.Parameter()
     stage = luigi.Parameter()
 
@@ -46,7 +104,8 @@ class PackageLogs(luigi.Task):
             return CheckJobStopped(self.job, self.launch_id)
 
     def output(self):
-        return ltarget(self.job, self.launch_id, self.stage)
+        return luigi.LocalTarget('{}/{}.zip'.format(LUIGI_STATE_FOLDER,
+                                                    target_name('02.logs', self.job, self.launch_id, self.stage)))
 
     def run(self):
         """Zips up all log/config. files and copies said archive to HDFS; finds the
@@ -56,27 +115,27 @@ class PackageLogs(luigi.Task):
             f.write('')
         self.output().remove()
         # What to remove from the paths:
-        chop = len(str(LOCAL_ROOT))
+        chop = len(str(LOCAL_PREFIX))
         with zipfile.ZipFile(self.output().path, 'w', allowZip64=True) as zipout:
-            for crawl_log in glob.glob("%s/%s/%s/crawl.log%s" % (
-            LOCAL_LOG_ROOT, self.job.name, self.launch_id, get_stage_suffix(self.stage))):
+            for crawl_log in remote_glob("%s/logs/%s/%s/crawl.log%s" % (
+                    LOCAL_OUTPUT_FOLDER, self.job.name, self.launch_id, get_stage_suffix(self.stage))):
                 logger.info("Found %s..." % os.path.basename(crawl_log))
                 zipout.write(crawl_log, arcname=crawl_log[chop:])
 
-            for log in glob.glob("%s/%s/%s/*-errors.log%s" % (
-            LOCAL_LOG_ROOT, self.job.name, self.launch_id, get_stage_suffix(self.stage))):
+            for log in remote_glob("%s/logs/%s/%s/*-errors.log%s" % (
+                    LOCAL_OUTPUT_FOLDER, self.job.name, self.launch_id, get_stage_suffix(self.stage))):
                 logger.info("Found %s..." % os.path.basename(log))
                 zipout.write(log, arcname=log[chop:])
 
-            for txt in glob.glob("%s/%s/%s/*.txt" % (LOCAL_JOBS_ROOT, self.job.name, self.launch_id)):
+            for txt in remote_glob("%s/%s/%s/*.txt" % (LOCAL_JOB_FOLDER, self.job.name, self.launch_id)):
                 logger.info("Found %s..." % os.path.basename(txt))
                 zipout.write(txt, arcname=txt[chop:])
 
-            for txt in glob.glob("%s/%s/%s/*.json" % (LOCAL_JOBS_ROOT, self.job.name, self.launch_id)):
+            for txt in remote_glob("%s/%s/%s/*.json" % (LOCAL_JOB_FOLDER, self.job.name, self.launch_id)):
                 logger.info("Found %s..." % os.path.basename(txt))
                 zipout.write(txt, arcname=txt[chop:])
 
-            cxml = "%s/%s/%s/crawler-beans.cxml" % (LOCAL_JOBS_ROOT, self.job.name, self.launch_id)
+            cxml = "%s/%s/%s/crawler-beans.cxml" % (LOCAL_JOB_FOLDER, self.job.name, self.launch_id)
             if os.path.exists(cxml):
                 logger.info("Found config...")
                 zipout.write(cxml, arcname=cxml[chop:])
@@ -91,7 +150,8 @@ class AssembleOutput(luigi.Task):
     description for each crawl stage.
     """
     task_namespace = 'output'
-    job = luigi.EnumParameter(enum=Jobs)
+    host = luigi.Parameter()
+    job = luigi.Parameter()
     launch_id = luigi.Parameter()
     stage = luigi.Parameter(default='final')
 
@@ -100,10 +160,11 @@ class AssembleOutput(luigi.Task):
             yield CheckJobStopped(self.job, self.launch_id)
         yield PackageLogs(self.job, self.launch_id, self.stage)
 
-    # TODO Move this into it's own job (atomicity):
+    # TODO Move this into it's own job? (atomicity):
     #    luigi.LocalTarget("%s/%s/%s/logs-%s.zip" % (self.LOCAL_LOG_ROOT, self.job.name, self.launch_id, self.stage))
     def output(self):
-        return otarget(self.job, self.launch_id, self.stage)
+        return luigi.LocalTarget(
+            '{}/{}'.format(LUIGI_STATE_FOLDER, target_name('03.outputs', self.job, self.launch_id, self.stage)))
 
     def run(self):
 
@@ -113,12 +174,12 @@ class AssembleOutput(luigi.Task):
         (warcs, viral) = self.parse_crawl_log(logs)
         # TODO Look for WARCs not spotted via the logs and add them in (ALSO allow this in the log parser)
         if self.stage == 'final':
-            for item in glob.glob(self.warc_file_path("*.warc.gz")):
+            for item in remote_glob(self.host, self.warc_file_path("*.warc.gz")):
                 if item not in warcs:
                     logger.info("Found additional WARC: %s" % item)
                     warcs.append(item)
             #
-            for item in glob.glob(self.viral_file_path("*.warc.gz")):
+            for item in remote_glob(self.host, self.viral_file_path("*.warc.gz")):
                 if item not in warcs:
                     logger.info("Found additional Viral WARC: %s" % item)
                     warcs.append(item)
@@ -131,9 +192,7 @@ class AssembleOutput(luigi.Task):
             # do some hard work here
             i += 1
             self.set_status_message = "Progress: Hashing WARC %d of %s" % (i, len(warcs))
-            hash_output = yield CalculateLocalHash(self.job, self.launch_id, warc)
-            # hash_file = yield HashLocalFile(warc)
-            # with hash_file.open('r') as f
+            hash_output = yield CalculateRemoteHash(self.job, self.launch_id, warc)
             with hash_output.open('r') as reader:
                 sha = reader.read().rstrip('\n')
             hashes[warc] = sha
@@ -162,13 +221,15 @@ class AssembleOutput(luigi.Task):
 
     def get_crawl_log(self):
         # First, parse the crawl log(s) and determine the WARC file names:
-        logfilepath = "%s/%s/%s/crawl.log%s" % (
-        LOCAL_LOG_ROOT, self.job.name, self.launch_id, get_stage_suffix(self.stage))
+        logfilepath = "%s/logs/%s/%s/crawl.log%s" % (LOCAL_OUTPUT_FOLDER, self.job.name,
+                                                     self.launch_id, get_stage_suffix(self.stage))
         logger.info("Looking for crawl logs stage: %s" % self.stage)
         logger.info("Looking for crawl logs: %s" % logfilepath)
         if os.path.exists(logfilepath):
             logger.info("Found %s..." % os.path.basename(logfilepath))
             return logfilepath
+        else:
+            raise Exception("Log file '%s' not found!" % logfilepath)
 
     def file_start_date(self, logs):
         """Finds the earliest timestamp in a series of log files."""
@@ -190,10 +251,10 @@ class AssembleOutput(luigi.Task):
         return timestamps[0].strftime("%Y-%m-%dT%H:%M:%SZ")
 
     def warc_file_path(self, warcfile):
-        return "%s/%s/%s/%s" % (WARC_ROOT, self.job.name, self.launch_id, warcfile)
+        return "%s/warcs/%s/%s/%s" % (LOCAL_OUTPUT_FOLDER, self.job.name, self.launch_id, warcfile)
 
     def viral_file_path(self, warcfile):
-        return "%s/%s/%s/%s" % (VIRAL_ROOT, self.job.name, self.launch_id, warcfile)
+        return "%s/viral/%s/%s/%s" % (LOCAL_OUTPUT_FOLDER, self.job.name, self.launch_id, warcfile)
 
     def parse_crawl_log(self, logs):
         """
@@ -228,13 +289,13 @@ class AssembleOutput(luigi.Task):
                 if 'warcFilename' in jmd:
                     warcfiles.add(jmd['warcFilename'])
                 elif 'warcPrefix' in jmd:
-                    for wren in glob.glob(
-                                    "%s/%s*.warc.gz*" % (h3().local_wren_folder, jmd['warcPrefix'])):
+                    for wren in remote_glob(
+                                    "%s/%s*.warc.gz*" % (LOCAL_WREN_FOLDER, jmd['warcPrefix'])):
                         if wren.endswith('.open'):
                             wren = wren[:-5]
                         warcfiles.add(os.path.basename(wren))
                     # Also check in case file has already been moved into output/warcs/{job}/{launch}:
-                    for wren in glob.glob( self.warc_file_path("%s*.warc.gz*" % jmd['warcPrefix'])):
+                    for wren in remote_glob( self.warc_file_path("%s*.warc.gz*" % jmd['warcPrefix'])):
                         warcfiles.add(os.path.basename(wren))
                     # FIXME Also look on HDFS for matching files?
                 else:
@@ -246,9 +307,9 @@ class AssembleOutput(luigi.Task):
             if self._file_exists(self.viral_file_path(warcfile)):
                 logger.info("Found Viral WARC %s" % self.viral_file_path(warcfile))
                 viral.append(self.viral_file_path(warcfile))
-            elif self._file_exists("%s/%s" % (h3().local_wren_folder, warcfile)):
+            elif self._file_exists("%s/%s" % (LOCAL_WREN_FOLDER, warcfile)):
                 logger.info("Found WREN WARC %s" % warcfile)
-                warcs.append("%s/%s" % (h3().local_wren_folder, warcfile))
+                warcs.append("%s/%s" % (LOCAL_WREN_FOLDER, warcfile))
             elif self._file_exists(self.warc_file_path(warcfile)):
                 logger.info("Found WARC %s" % self.warc_file_path(warcfile))
                 warcs.append(self.warc_file_path(warcfile))
@@ -283,7 +344,8 @@ class AggregateOutputs(luigi.Task):
     from them.
     """
     task_namespace = 'output'
-    job = luigi.EnumParameter(enum=Jobs)
+    host = luigi.Parameter()
+    job = luigi.Parameter()
     launch_id = luigi.Parameter()
     state = luigi.Parameter()
     outputs = luigi.ListParameter()
@@ -297,7 +359,9 @@ class AggregateOutputs(luigi.Task):
             yield AssembleOutput(self.job, self.launch_id, stage)
 
     def output(self):
-        return atarget(self.job, self.launch_id, "%s.%s" %(len(self.outputs), self.state))
+        return luigi.LocalTarget(
+                '{}/{}'.format(LUIGI_STATE_FOLDER, target_name('04.assembled', self.job, self.launch_id,
+                                                               "%s.%s" %(len(self.outputs), self.state))))
 
     def run(self):
         # Sort inputs by checkpoint timestamp and merge into versioned lists:
@@ -335,15 +399,18 @@ class ProcessOutputs(luigi.Task):
 
     """
     task_namespace = 'output'
-    job = luigi.EnumParameter(enum=Jobs)
+    host = luigi.Parameter()
+    job = luigi.Parameter()
     launch_id = luigi.Parameter()
+    final_package_only = luigi.BoolParameter(default=True)
 
     def requires(self):
         # FIXME Need to make sure this copes with crawl.log.TIMESTAMP etc. from failures.
         # Look for checkpoints to package, and package them in the correct order:
         outputs = {}
         is_final = False
-        for item_path in glob.glob("%s/%s/%s/crawl.log*" % (LOG_ROOT, self.job.name, self.launch_id)):
+        for item_path in remote_glob(self.host,
+                                     "%s/logs/%s/%s/crawl.log*" % (LOCAL_OUTPUT_FOLDER, self.job.name, self.launch_id)):
             item = os.path.basename(item_path)
             logger.info("ITEM %s" % item)
             if item == "crawl.log":
@@ -357,13 +424,19 @@ class ProcessOutputs(luigi.Task):
         output_list = sorted(outputs.keys())
         logger.info("Ordered by date: %s" % output_list)
 
+        # Build up the list of stages, in order, and aggregate:
         aggregate = list()
         for key in output_list:
             aggregate.append(outputs[key])
-            yield AggregateOutputs(self.job, self.launch_id, key, aggregate)
+            if not self.final_package_only:
+                yield AggregateOutputs(self.job, self.launch_id, key, aggregate)
+        # Only output the final package (not this intermediate ones):
+        if self.final_package_only:
+            yield AggregateOutputs(self.job, self.launch_id, "final", aggregate)
 
     def output(self):
-        return atarget(self.job, self.launch_id, "list")
+        return luigi.LocalTarget(
+                '{}/{}'.format(LUIGI_STATE_FOLDER, target_name('04.assembled', self.job, self.launch_id, "list")))
 
     def run(self):
         logger.info(self.launch_id)
@@ -382,6 +455,39 @@ class ProcessOutputs(luigi.Task):
             yield CheckJobStopped(self.job, self.launch_id)
 
 
+class ScanForLaunches(luigi.WrapperTask):
+    """
+    This task scans the output folder for jobs and instances of those jobs, looking for crawled content to process.
+
+    Sub-class this and override the scan_job_launch method as needed.
+    """
+    task_namespace = 'scan'
+    host = luigi.Parameter()
+    date_interval = luigi.DateIntervalParameter(
+        default=[datetime.date.today() - datetime.timedelta(days=1), datetime.date.today()])
+    timestamp = luigi.DateMinuteParameter(default=datetime.datetime.today())
+
+    def requires(self):
+        # Enumerate the jobs:
+        for (host, job, launch) in self.enumerate_launches():
+            logger.info("Processing %s:%s/%s" % ( host, job, launch ))
+            yield self.scan_job_launch(host, job, launch)
+
+    def enumerate_launches(self):
+        # Look for jobs that need to be processed:
+        for date in self.date_interval:
+            for job_item in remote_glob(self.host, "%s/*" % LOCAL_JOB_FOLDER):
+                job = os.path.basename(job_item)
+                if os.path.isdir(job_item):
+                    launch_glob = "%s/%s*" % (job_item, date.strftime('%Y%m%d'))
+                    logger.info("Looking for job launch folders matching %s" % launch_glob)
+                    for launch_item in remote_glob(self.host, launch_glob):
+                        logger.info("Found %s" % launch_item)
+                        if os.path.isdir(launch_item):
+                            launch = os.path.basename(launch_item)
+                            yield (self.host, job, launch)
+
+
 class ScanForOutputs(ScanForLaunches):
     """
     This task scans the output folder for jobs and instances of those jobs, looking for crawled content to process.
@@ -389,25 +495,9 @@ class ScanForOutputs(ScanForLaunches):
     task_namespace = 'scan'
     scan_name = 'output'
 
-    def scan_job_launch(self, job, launch):
-        return ProcessOutputs(job, launch)
-
-
-@AssembleOutput.event_handler(luigi.Event.SUCCESS)
-def notify_success(task):
-    """Will be called directly after a failed execution
-       of `run` on any JobTask subclass
-    """
-    if slack().token and task.stage == 'final':
-        sc = SlackClient(slack().token)
-        print(sc.api_call(
-            "chat.postMessage", channel="#crawls",
-            text="Job %s finished and packaged successfully! :tada:"
-                 % format_crawl_task(task), username='crawljobbot'))
-        # , icon_emoji=':robot_face:'))
-    else:
-        logger.warning("No Slack auth token set, no message sent.")
+    def scan_job_launch(self, host, job, launch):
+        return ProcessOutputs(host, job, launch)
 
 
 if __name__ == '__main__':
-    luigi.run(['scan.ScanForOutputs', '--date-interval', '2016-11-01-2016-11-10', '--local-scheduler'])
+    luigi.run(['scan.ScanForOutputs', '--host', 'crawler07.bl.uk' '--date-interval', '2017-02-06-2017-02-08', '--local-scheduler'])
