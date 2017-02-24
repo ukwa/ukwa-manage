3f14a898873831ac9c477f2b62b7cb9438c7e706
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-02-24 14:25:50 +0000

    Use proper re.split.

--- tasks/process/log_analysis_hadoop.py
+++ tasks/process/log_analysis_hadoop.py
@@ -25,7 +25,7 @@ class CrawlLogLine(object):
             self.annotation_string) = re.split(" +", line.strip(), maxsplit=11)
         # Account for any JSON 'extra info' ending:
         if ' {"' in self.annotation_string and self.annotation_string.endswith('"}'):
-            self.annotation_string, self.extra_json = self.annotation_string.split(' {"', maxsplit=1)
+            self.annotation_string, self.extra_json = re.split(re.escape(' {"'), self.annotation_string, maxsplit=1)
             self.extra_json = '{"%s' % self.extra_json
         # And split out the annotations:
         self.annotations = self.annotation_string.split(',')
