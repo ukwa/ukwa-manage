ce3856de8170e911d705414a60f66e5b8ff770dc
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-03-01 21:10:10 +0000

    Fix broken append syntax!

--- tasks/process/log_analysis_hadoop.py
+++ tasks/process/log_analysis_hadoop.py
@@ -289,7 +289,7 @@ class AnalyseLogFile(luigi.contrib.hadoop.JobTask):
         :return:
         """
         jcs = super(AnalyseLogFile, self).jobconfs()
-        jcs.append('mapred.map.tasks', 100)
+        jcs.append('mapred.map.tasks=%s' % 100)
         #jcs.append('mapred.min.split.size', ) mapred.max.split.size, in bytes. e.g. 256*1024*1024 = 256M
         return jcs
 
