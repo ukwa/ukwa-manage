f6432d8516c1f402a4f8aaf1f703f08023000fb8
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-02-26 15:49:46 +0000

    Add summary module.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,8 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode'
+    'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
