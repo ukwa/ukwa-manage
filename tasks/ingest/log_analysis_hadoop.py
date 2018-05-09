329273bfa33fea70ee3757cdabf3f0343a962e46
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-05-09 15:34:19 +0100

    Added more dependencies.

--- tasks/ingest/log_analysis_hadoop.py
+++ tasks/ingest/log_analysis_hadoop.py
@@ -8,8 +8,8 @@ import luigi.contrib.hdfs
 import luigi.contrib.hadoop
 from luigi.contrib.hdfs.format import Plain, PlainDir
 
-import lib # Imported so extra_modules MR-bundle can access the following:
-import surt, tldextract, idna, requests
+import lib # Imported so extra_modules MR-bundle can access them
+import surt, tldextract, idna, requests, urllib3, certifi, chardet # Unfortunately the surt module has a LOT of dependencies.
 
 logger = logging.getLogger(__name__)
 
@@ -284,7 +284,7 @@ class AnalyseLogFile(luigi.contrib.hadoop.JobTask):
             return luigi.LocalTarget(path=out_name)
 
     def extra_modules(self):
-        return [lib,surt,tldextract,idna,requests]
+        return [lib,surt,tldextract,idna,requests,urllib3,certifi,chardet]
 
     def init_mapper(self):
         # Set up...
@@ -377,7 +377,7 @@ class SummariseLogFiles(luigi.contrib.hadoop.JobTask):
             return luigi.LocalTarget(path=out_name)
 
     def extra_modules(self):
-        return [lib]
+        return [lib,surt,tldextract,idna,requests,urllib3,certifi,chardet]
 
     def mapper(self, line):
         log_time, status, size, url, discovery_path, referrer, mime, thread, request_time, hash, ignore, annotations \
