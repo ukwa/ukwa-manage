2c5f9956f9d16857d94c57fc5fc7ab25f505419f
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-05-16 15:19:18 +0100

    Add more reports.

--- tasks/ingest/reports.py
+++ tasks/ingest/reports.py
@@ -5,6 +5,27 @@ from tasks.ingest.listings import ListParsedPaths
 from lib.targets import ReportTarget, TaskTarget
 
 
+# Utility for pretty-printing in e.g. TB (not TiB):
+def humanbytes(B):
+    '''Return the given bytes as a human friendly KB, MB, GB, or TB string'''
+    B = float(B)
+    KB = float(1000)
+    MB = float(KB ** 2)
+    GB = float(KB ** 3)
+    TB = float(KB ** 4)
+
+    if B < KB:
+        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B/KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B/MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B/GB)
+    elif TB <= B:
+        return '{0:.2f} TB'.format(B/TB)
+
+
 class GenerateHDFSReports(luigi.Task):
     """
     Generate a set of reports based on HDFS content
@@ -34,9 +55,33 @@ class GenerateHDFSReports(luigi.Task):
                 # Pandas query:
                 df2 = df.groupby([df.collection, df.stream, df.timestamp.dt.year, df.kind]).file_size.sum().unstack()
                 # Output the result as CSV:
-                df2.to_csv(f_out)
+                df2.to_csv(f_out,float_format="%.0f")
 
-        # Tag all as done:
+            # Focus on NPLD:
+            np = df.loc[df.collection == 'npld'].loc[df.kind.isin(['warcs', 'crawl-logs', 'viral'])].reset_index()
+
+            # NPLD By year, humanbytes:
+            out = ReportTarget('content/reports/hdfs', 'npld-total-file-size-by-stream-per-year.csv')
+            with out.open('w') as f_out:
+                npsy = np.groupby([np.timestamp.dt.year, np.stream]).file_size.sum().apply(humanbytes).unstack()
+                npsy.to_csv(f_out)
+
+            # NPLD By month:
+            out = ReportTarget('content/reports/hdfs', 'npld-total-file-size-by-stream-per-month.csv')
+            with out.open('w') as f_out:
+                npsm = np.groupby([np.timestamp.dt.to_period('M'), np.stream]).file_size.sum().unstack()
+                npsm.to_csv(f_out,float_format="%.0f")
+
+            # NPLD Total:
+            out = ReportTarget('content/reports/hdfs', 'npld-total-file-size-by-stream-totals.csv')
+            with out.open('w') as f_out:
+                totals = np.groupby(np.stream).file_size.sum().reset_index()
+                totals = totals.append({'stream': 'total', 'file_size': totals.file_size.sum()}, ignore_index=True)
+                totals.file_size = totals.file_size.apply(humanbytes)
+                totals.to_csv(f_out,index=False)
+
+
+            # Tag all as done:
         with self.output().open('w') as f_out:
             f_out.write("DONE")
 
