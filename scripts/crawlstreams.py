869fdaa6c9831759030bea432e4225ee7cdd3ae0
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-06-13 23:43:28 +0100

    Added summariser for discovered-url streams.

--- scripts/crawlstreams.py
+++ scripts/crawlstreams.py
@@ -1,4 +1,5 @@
 import sys
+from urlparse import urlparse
 import json
 import argparse
 import logging
@@ -21,26 +22,18 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def show_raw_stream(queue='uris.tocrawl.fc', starting_at='earliest', bootstrap_servers=None):
-    # Defaults:
-    if bootstrap_servers is None:
-        bootstrap_servers = ['localhost:9092']
-
-    # To consume latest messages and auto-commit offsets
-    consumer = KafkaConsumer(queue, auto_offset_reset=starting_at,
-                             bootstrap_servers=bootstrap_servers)
+def show_raw_stream(consumer):
     for message in consumer:
         # message value and key are raw bytes -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
         j = json.loads(message.value.decode('utf-8'))
         if 'parentUrl' in j:
             # This is a discovered URL stream:
-            print("%s:%d:%d: %-80s via %-80s" % (message.topic, message.offset,
-                                                  message.partition, j['url'][-80:], j['parentUrl'][-80:]))
+            print("%010d:%04d: %-80s via %-80s" % (message.offset, message.partition,
+                                                j['url'][-80:], j['parentUrl'][-80:]))
         elif 'status_code' in j:
             # This is a crawled-event stream:
-            print("%s:%d:%d: %-80s via %-80s" % (message.topic, message.offset,
-                                                 message.partition , j['url'][-80:], j.get('via', 'NONE')[-80:]))
+            print("%s %-80s via %-80s" % (j['timestamp'], j['url'][-80:], j.get('via', 'NONE')[-80:]))
         else:
             # This is unknown!
             logger.error("Unrecognised stream! %s" % message.value)
@@ -50,17 +43,47 @@ def show_raw_stream(queue='uris.tocrawl.fc', starting_at='earliest', bootstrap_s
             return
 
 
+def summarise_stream(consumer, max_messages=-1):
+    tot = {}
+    msg_count = 0
+    for message in consumer:
+        msg_count += 1
+        if max_messages > 0 and msg_count > max_messages:
+            break
+        j = json.loads(message.value.decode('utf-8'))
+        if 'parentUrl' in j:
+            url = j['url']
+            via = j['parentUrl']
+            urlp = urlparse(url)
+            viap = urlparse(via)
+            stats = tot.get(urlp.hostname,{})
+            if viap.hostname != urlp.hostname and not 'via' in stats:
+                stats['via'] = via
+                print(j) # hop, isSeed, parentUrlMetadata.pathFromSeed
+            stats['tot'] = stats.get('tot', 0) + 1
+            tot[urlp.hostname] = stats
+    print("URL Host\tDiscovered Via\tTotal URLs")
+    for host in tot:
+        print("%s\t%s\t%i" %(host, tot[host].get('via', '-'), tot[host]['tot']))
+
 
 def main(argv=None):
     parser = argparse.ArgumentParser('(Re)Launch URIs into crawl queues.')
-    parser.add_argument('-k', '--kafka-bootstrap-server', dest='kafka_server', type=str, default="localhost:9092",
+    parser.add_argument('-k', '--kafka-bootstrap-server', dest='bootstrap_server', type=str, default="localhost:9092",
                         help="Kafka bootstrap server(s) to use [default: %(default)s]")
+    parser.add_argument('-o', '--offset', dest='starting_at', type=str, default="earliest",
+                        help="Whether to start at the earliest or latest message [default: %(default)s]")
     parser.add_argument("-q", "--queue", dest="queue", default="uris.crawled.fc", required=False,
                         help="Name of queue to inspect. [default: %(default)s]")
 
     args = parser.parse_args()
     #
-    show_raw_stream(queue=args.queue, bootstrap_servers=args.kafka_server)
+
+    # To consume latest messages and auto-commit offsets
+    consumer = KafkaConsumer(args.queue, auto_offset_reset=args.starting_at,
+                             bootstrap_servers=args.bootstrap_server,
+                             consumer_timeout_ms=1*1000)
+    summarise_stream(consumer)
 
 
 if __name__ == "__main__":
