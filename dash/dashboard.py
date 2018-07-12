027d2ee37782d6725b4691a94b332a28d5a5c6e3
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-07-13 00:01:37 +0100

    Switch to simple global file cache for storing the report object.

--- dash/dashboard.py
+++ dash/dashboard.py
@@ -2,12 +2,14 @@ import os
 import json
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from flask import Flask
-from flask import render_template, redirect, url_for, flash, jsonify, session
+from flask import render_template, redirect, url_for, flash, jsonify
+from werkzeug.contrib.cache import FileSystemCache
 from lib.heritrix3.collector import Heritrix3Collector
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = os.environ.get('APP_SECRET', 'dev-mode-key')
+cache = FileSystemCache('/__cache__')
 
 
 @app.route('/')
@@ -35,10 +37,11 @@ def control_all(action=None):
     try:
         c = Heritrix3Collector()
         services = c.do(action)
-        session[action] = services
+        app.logger.info(json.dumps(services, indent=2))
+        cache.add(action, services)
 
     except Exception as e:
-        flash("Something went wrong!\n%s" % e)
+        flash("Something went wrong!\n%s" % e.message)
         return redirect(url_for('status'))
 
     return redirect(url_for('result_all', action=action))
@@ -47,13 +50,13 @@ def control_all(action=None):
 @app.route('/result/all/<action>')
 def result_all(action=None):
     try:
-        services = session.pop(action)
+        services = cache.get(action)
+        app.logger.info(json.dumps(services, indent=2))
         return jsonify(services)
 
     except Exception as e:
         flash("Something went wrong!\n%s" % e.message)
         return redirect(url_for('status'))
-
 
 
 if __name__ == "__main__":
