86191a4dc98db1488b348995d981158a38b7995f
Lee <lee.davey@bl.uk>
2019-06-05 10:43:01 +0100

    52 Status Code Stats - Counts

--- tasks/ingest/log_analysis_tests.py
+++ tasks/ingest/log_analysis_tests.py
@@ -1,6 +1,7 @@
 import json
 import luigi
-from tasks.ingest.log_analysis_hadoop import SummariseLogFiles, ListDeadSeeds
+from tasks.ingest.log_analysis_hadoop import SummariseLogFiles, ListDeadSeeds, CountStatusCodes
+
 
 def test_run_summariser():
     task = SummariseLogFiles( [ '../../test/fragment-of-a-crawl.log' ], 'dc','20170220090024', False )
@@ -30,4 +31,16 @@ def test_run_dead_seeds_list():
 
     assert count is 2
 
+    
+def test_run_count_status_codes():
+    task = CountStatusCodes( [ '../../test/fragment-of-a-crawl.log' ], 'dc','20170220090024', False )
+    luigi.build([task], local_scheduler=True)
 
+    count = 0
+    with task.output().open() as f:
+        for line in f.readlines():
+            key, val = line.split('\t',1)
+            print(key, val)
+            count = count + 1
+
+    assert count is 6
