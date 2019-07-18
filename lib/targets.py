2fd743e6c711cf71d83b44cc00000bf4988db961
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-07-18 13:58:05 +0100

    Ensure commit is quickly visible for state management.

--- lib/targets.py
+++ lib/targets.py
@@ -1,4 +1,5 @@
 import os
+import pysolr
 import logging
 import posixpath
 import luigi
@@ -221,3 +222,39 @@ class DatedStateFileTask(luigi.Task):
         return True
 
 
+class TrackingDBStatusField(luigi.Target):
+
+    DEFAULT_TRACKDB = os.environ.get('TRACKING_DB_SOLR_URL', 'http://localhost:8983/solr/tracking')
+
+    def __init__(
+        self, doc_id, field, value, trackdb=None
+    ):
+        """
+        Args:
+            doc_id (str): The ID of the document to update
+            field (str): The field to use to record the status
+            value (str): The value the field should hold to indicate task completion
+            trackdb (str): URL of the Solr tracking database
+        """
+        self.trackdb = trackdb or self.DEFAULT_TRACKDB
+        self.doc_id = doc_id
+        self.field = field
+        self.value = value
+
+        # Setup connection:
+        self.solr = pysolr.Solr(self.trackdb, always_commit=True)
+
+    def exists(self):
+        result = self.solr.search(q='id:"%s" AND %s:"%s"' % (self.doc_id, self.field, self.value))
+        if result.hits == 0:
+            return False
+        else:
+            return True
+
+    def touch(self):
+        doc = {'id': self.doc_id, self.field: self.value}
+        result = self.solr.add([doc], fieldUpdates={self.field: 'set'}, softCommit=True)
+        print(result)
+
+    def open(self, mode):
+        raise NotImplementedError("Cannot open() TrackingDBStatusField")
