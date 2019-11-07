1a93a446c645fb59cb401aad9c88ea75f924cd20
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-11-07 15:26:46 +0000

    Skip keys.

--- tasks/crawl/w3act.py
+++ tasks/crawl/w3act.py
@@ -161,7 +161,7 @@ class CollectionList(luigi.Task):
             w3a = json.load(f_in)
 
         # Persist to disk:
-        collections = w3a['collections']
+        collections = w3a['collections'].values()
         with self.output().open('w') as f:
             f.write('{}'.format(json.dumps(collections , indent=4)))
 
@@ -187,7 +187,7 @@ class SubjectList(luigi.Task):
             w3a = json.load(f_in)
 
         # Persist to disk:
-        subjects = w3a['subjects']
+        subjects = w3a['subjects'].values()
         with self.output().open('w') as f:
             f.write('{}'.format(json.dumps(subjects, indent=4)))
 
@@ -213,7 +213,7 @@ class TargetList(luigi.Task):
             w3a = json.load(f_in)
 
         # Persist to disk:
-        targets = w3a['targets']
+        targets = w3a['targets'].values()
         with self.output().open('w') as f:
             f.write('{}'.format(json.dumps(targets, indent=4)))
 
