42e026594c0397912aa78241b7f7eab42e2c7739
Gil Hoggarth <gil.hoggarth@bl.uk>
2020-02-13 14:33:33 +0000

    Added presumed looping through warc list but problematic

--- tasks/access/update_solr_index.py
+++ tasks/access/update_solr_index.py
@@ -9,9 +9,9 @@ import luigi.contrib.ssh
 
 from tasks.access.hdfs_list_warcs import ListWarcsForDateRange
 from lib.webhdfs import WebHdfsPlainFormat
+from lib.targets import TrackingDBStatusField
 
 logger = logging.getLogger('luigi-interface')
-
 
 class CopyToRemote(luigi.Task):
 	# Copy file argument to destination directory argument on server argument
@@ -20,17 +20,11 @@ class CopyToRemote(luigi.Task):
 	mapred_host = luigi.Parameter()
 
 	def run(self):
-		logger.info("Uploading {} to {};{}".format(self.input_file, self.mapred_host, self.dest_dir))
 		self.output().put(self.input_file)
 
 	def output(self):
 		fpfile = self.dest_dir + os.path.basename(self.input_file)
 		return luigi.contrib.ssh.RemoteTarget(path=fpfile, host=self.mapred_host)
-
-#class SolrVerify(luigi.Task):
-#	warcs_file = luigi.Parameter()
-#	solr_api = luigi.Parameter()
-#	task_namespace = "access.index"
 
 class SolrIndexer(luigi.contrib.hadoop_jar.HadoopJarJobTask):
 	warcs_file = luigi.Parameter()
@@ -81,12 +75,42 @@ class SolrIndexer(luigi.contrib.hadoop_jar.HadoopJarJobTask):
 			"-c", warc_config_fpfile,
 			"-i", self.input().path,
 			"-o", output_dir,
-			"-a"
+			"-a",
+			"-w"
 		]
 
 	def output(self):
-		full_path = self.hdfs_processing_dir + self.ymdhms + '/_SUCCESS'
-		return luigi.contrib.hdfs.HdfsTarget(full_path, format=WebHdfsPlainFormat(use_gzip=False))
+		return luigi.contrib.hdfs.HdfsTarget(path=self.hdfs_processing_dir + self.ymdhms + '/_SUCCESS', format=luigi.contrib.hdfs.PlainFormat())
+
+
+class SolrVerify(luigi.Task):
+	warc = luigi.Parameter()
+	solr_api = luigi.Parameter()
+	task_namespace = "access.index"
+
+	def run(self):
+		with open(self.output().path, 'w') as vo:
+			logger.debug("==== writing out {}".format(self.output().path))
+			vo.write("WARC verified to be in Solr - {}".format(self.warc))
+
+	def output(self):
+		return luigi.LocalTarget("/tmp/luigi/{}-solr_verified".format(os.path.basename(self.warc.strip())))
+
+
+class MarkWarc(luigi.Task):
+	warc = luigi.Parameter()
+	tracking_db_url = luigi.Parameter()
+	stream = luigi.Parameter()
+	year = luigi.Parameter()
+	status_field = luigi.Parameter()
+
+	def input(self):
+		logger.debug("==== MarkWarc input: {}-{}".format(self.stream, self.year))
+		return self.stream + '-' + self.year
+
+	def output(self):
+		fp_warc = 'hdfs://hdfs:54310' + self.warc
+		return TrackingDBStatusField(doc_id=fp_warc, field=self.status_field, value=self.input(), trackdb=self.tracking_db_url)
 
 
 class SolrIndexAndVerify(luigi.Task):
@@ -101,6 +125,11 @@ class SolrIndexAndVerify(luigi.Task):
 	warc_indexer_jar = luigi.Parameter()
 	hdfs_processing_dir = luigi.Parameter()
 	solr_api = luigi.Parameter()
+	verify_only = luigi.BoolParameter(default=False)
+
+	# globals for looping through warcs in warc list
+	warc_num = 0
+	warc_line = ''
 	# task_namespace defines scope of class. Without defining this, other classes
 	# could call this class inside their scope, which would be wrong.
 	task_namespace = "access.index"
@@ -108,6 +137,9 @@ class SolrIndexAndVerify(luigi.Task):
 	# status_field is set here as I don't expect it to change in any implementation, 
 	# whereas the other fields are parameters to allow triggering script configuration.
 	status_field = 'solr_index_ss'
+
+	# Boolean to capture any process failures
+	solr_index_verify_mark_fail = False
 
 	# Get list of WARCs that don't have flag set indicating already indexed in Solr
 	def requires(self):
@@ -117,7 +149,7 @@ class SolrIndexAndVerify(luigi.Task):
 			stream=self.stream,
 			status_field=self.status_field,
 			status_value=self.stream + '-' + self.year,
-			limit=20,
+			limit=10,
 			tracking_db_url=self.tracking_db_url
 		)
 
@@ -128,41 +160,67 @@ class SolrIndexAndVerify(luigi.Task):
 		# test some data received
 		listsize = os.stat(self.input().path)
 		if listsize.st_size == 0:
-			logger.warning("No WARCs listed for {} {}".format(self.stream, self.year))
-			self.output().touch()
+			logger.warning("==== No WARCs listed for {} {}".format(self.stream, self.year))
+			with open(self.output().path, 'w') as out:
+				out.write("No WARCs listed for {} {}".format(self.stream, self-year))
 
 		else:
-			# Submit MapReduce job of WARCs list for Solr search service
-			logger.info("Submitting WARCs for {} {} into {}".format(self.stream, self.year, self.solr_api))
-			logger.info("List of WARCs to be submitted: {}".format(self.input().path))
-			solr_index_task = SolrIndexer(
-				warcs_file=self.input().path, stream=self.stream, year=self.year, solr_api=self.solr_api,
-				mapred_host=self.mapred_host, mapred_user=self.mapred_user, mapred_dir=self.mapred_dir,
-				annotations=self.annotations, whitelist=self.whitelist,  warc_indexer_jar=self.warc_indexer_jar,
-				hdfs_processing_dir=self.hdfs_processing_dir
-			)
-			yield solr_index_task
-			if not solr_index_task.complete():
-				logger.error("SOMETHING WENT WRONG WITH SOLR INDEXING")
+			if not self.verify_only:
+				# Submit MapReduce job of WARCs list for Solr search service
+				logger.info("---- Submitting WARCs for {} {} into {}".format(self.stream, self.year, self.solr_api))
+				logger.info("---- List of WARCs to be submitted: {}".format(self.input().path))
+				solr_index_task = SolrIndexer(
+					warcs_file=self.input().path, stream=self.stream, year=self.year, solr_api=self.solr_api,
+					mapred_host=self.mapred_host, mapred_user=self.mapred_user, mapred_dir=self.mapred_dir,
+					annotations=self.annotations, whitelist=self.whitelist,  warc_indexer_jar=self.warc_indexer_jar,
+					hdfs_processing_dir=self.hdfs_processing_dir
+				)
+				yield solr_index_task
 
-			else:
-				# Verifying WARCs in Solr
-				logger.info("Verifying submitted WARCs in Solr")
-				solr_verify_task = SolrVerify(warcs_file=self.input().path, solr_api=self.solr_api)
-				yield solr_verify_task
+				if not solr_index_task.complete():
+					logger.error("==== Solr index of {warclist} into {stream}-{year} failed {now}".format(
+						now=datetime.datetime.now(), stream=self.stream, year=self.year, warclist=self.input().path))
+					self.solr_index_verify_mark_fail = True
 
-				if not solr_verify_task.complete():
-					logger.error("SOMETHING WENT WRONG WITH SOLR INDEX VERIFYING")
+			# If mapreduce job into solr succeeded, loop through each WARC in list
+			if not self.solr_index_verify_mark_fail:
+				logger.info("---- Verifying submitted WARCs in Solr")
+				with open(self.input().path, 'r') as wlh:
+					for self.warc_line in wlh:
+						self.warc_num += 1
+						logger.info("---- {}) Verifying WARC {}".format(self.warc_num, self.warc_line))
+						# Verify WARC in Solr
+						solr_verify_task = SolrVerify(warc=self.warc_line, solr_api=self.solr_api)
+						yield solr_verify_task
 
-				# Mark WARCs in trackdb as indexed into Solr
+						logger.debug("==== beyond index, verify")
+						if not solr_verify_task.complete():
+							logger.error("==== Solr verify of {warc} run into {stream}-{year} failed {now}".format(
+								now=datetime.datetime.now(), stream=self.stream, year=self.year, warc=self.warc_line))
+							self.solr_index_verify_mark_fail = True
+						else:
+							# Mark WARCs in trackdb as indexed into Solr
+							logger.info("---- Marking WARC as indexed {}".format(self.warc_line))
+							mark_warc_task = MarkWarc(warc=self.warc_line, tracking_db_url=self.tracking_db_url,
+								stream=self.stream, year=self.year, status_field=self.status_field)
+							yield mark_warc_task
 
-				# mark luigi task as done
-				self.output().touch()
+							if not mark_warc_task.complete():
+								logger.error("==== Solr mark of {warc} run into {stream}-{year} failed {now}".format(
+									now=datetime.datetime.now(), stream=self.stream, year=self.year, warc=self.warc_line))
+								self.solr_index_verify_mark_fail = True
+
+			logger.debug("==== beyond index, verify, and mark")
+			# If all steps, index, verify, and mark - succeeded, create luigi script output file
+			if not self.solr_index_verify_mark_fail:
+				logger.debug("---- SolrIndexAndVerify completed")
+				with open(self.output().path, 'w') as out:
+					out.write("Solr index, verify & mark run into {stream}-{year} completed {now}".format(
+						now=datetime.datetime.now(), stream=self.stream, year=self.year))
 
 	def output(self):
-		logger.debug('---- SolrIndexAndVerify output: ----------------------------------')
-		return luigi.LocalTarget("/tmp/{}".format(self.stream + '-' + self.year))
+		return luigi.LocalTarget("/tmp/luigi/{}-solr_indexed_verified_marked".format(self.stream + '-' + self.year))
 
 
 if __name__ == '__main__':
-	luigi.run(['access.index.SolrIndexAndVerify', '--workers', '4'])
+	luigi.run(['access.index.SolrIndexAndVerify', '--workers', '5'])
