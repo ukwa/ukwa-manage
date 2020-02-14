9e31509f6d1d097317cc46937f144e432c3c7adb
root <root@dev1.n45.wa.bl.uk>
2020-02-14 16:17:52 +0000

    Corrected dev trackdb data dir

--- dev/start_solr_trackdb.sh
+++ dev/start_solr_trackdb.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-export DATA_TRACKDB=/mnt/nfs/data/
+export DATA_TRACKDB=/mnt/nfs/data/trackdata
+
 # start the solr trackdb container
 docker stack deploy -c ../docker-compose.yml trackdb
