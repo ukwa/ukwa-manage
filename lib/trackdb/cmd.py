bb0d7fc4387cb08f76a1a19ee31e35556be269c8
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-04-03 16:48:20 +0100

    Add initial get and update syntax.

--- lib/trackdb/cmd.py
+++ lib/trackdb/cmd.py
@@ -2,8 +2,12 @@
 This contains the core TrackDB code for managing queries and updates to the Tracking Database
 '''
 import os
+import json
+import logging
 import argparse
-from solr import SolrTrackDB
+from lib.trackdb.solr import SolrTrackDB
+
+logger = logging.getLogger(__name__)
 
 # Defaults to using the DEV TrackDB Solr backend:
 DEFAULT_TRACKDB = os.environ.get("TRACKDB_URL","http://trackdb.dapi.wa.bl.uk/solr/tracking")
@@ -16,12 +20,25 @@ def main():
     parser.add_argument('-t', '--trackdb-url', type=str, help='The TrackDB URL to talk to (defaults to %s).' % DEFAULT_TRACKDB, 
         default=DEFAULT_TRACKDB)
     parser.add_argument('--dry-run', action='store_true', help='Do not modify the TrackDB.')
+    parser.add_argument('-i', '--indent', type=int, help='Number of spaces to indent when emitting JSON.')
+    parser.add_argument('--filter-by-stream', 
+        choices= ['frequent', 'domain', 'webrecorder'], 
+        help='Filter the results by stream.', default='[* TO *]')
+    parser.add_argument('--filter-by-collection', 
+        choices= ['npld', 'bypm'], 
+        help='Filter the results by the collection, NPLD or by-permission.', default='[* TO *]')
+    parser.add_argument('--filter-by-year', 
+        type=int,
+        help='Filter down by date.')
+    parser.add_argument('--filter-by-field', 
+        type=str,
+        help='Filter by any additional field and value, in the form field:value.')
     parser.add_argument('kind', 
-        choices= ['files', 'warcs', 'logs', 'launches'], 
-        help='The kind of thing to track.')
+        choices= ['warcs', 'logs', 'launches'], 
+        help='The kind of thing to track.', default='[* TO *]')
 
     # Use sub-parsers for different operations:
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest="op")
 
     # Add a parser for the 'get' subcommand:
     parser_get = subparsers.add_parser('get', help='Get a single record from the TrackDB.')
@@ -29,24 +46,44 @@ def main():
 
     # Add a parser for the 'list' subcommand:
     parser_list = subparsers.add_parser('list', help='Get a list of records from the TrackDB.')
-    parser_get.add_argument('--limit', type=int, default=10, help='The maximum number of records to return.')
+    parser_list.add_argument('--limit', type=int, default=10, help='The maximum number of records to return.')
 
     # Add a parser for the 'update' subcommand:
     parser_up = subparsers.add_parser('update', help='Create or update on a record in the TrackDB.')
-    parser_up.add_argument('--set', type=str, help='Set a field to a given value, using the format "field:value".', nargs="*")
-    parser_up.add_argument('--add', type=str, help='Add the given value to a field, using the format "field:value".', nargs="*")
-    parser_up.add_argument('--remove', type=str, help='Remove the specified field.', nargs="*")
-    parser_up.add_argument('--inc', type=str, help='Increment the specified field, using the format "field:increment" (the increment defaults to 1 if not specified).', nargs="*")
+    parser_up.add_argument('--set', metavar=('field','value'), help='Set a field to a given value.', nargs=2)
+    parser_up.add_argument('--add', metavar=('field','value'), help='Add the given value to a field. Always uses add-distinct', nargs=2)
+    parser_up.add_argument('--remove', metavar=('field','value'), help='Remove the specified value from the field.', nargs=2)
+    parser_up.add_argument('--inc', metavar=('field','increment'), help='Increment the specified field, e.g. "--inc counter 1".', nargs=2)
     parser_up.add_argument('id', type=str, help='The record ID to use.')
+
+# trackdb warcs update --set cdx_index_ss data-heritrix_unverified hdfs://identifier
+# trackdb warcs update --remove cdx_index_ss data-heritrix_unverified --set cdx_index_ss data-heritrix hdfs://identifier
 
     # And PARSE it:
     args = parser.parse_args()
 
     # Set up Solr client:
     tdb = SolrTrackDB(args.trackdb_url, kind=args.kind)
-    docs = tdb.list('[* TO *]', '[* TO *]', 'test_s', 'test')
-    print(docs)
-    tdb.update('anj-test','kind_s','warcs')
+
+    # Ops:
+    logger.info("Got args: %s" % args)
+    if args.op == 'list':
+        docs = tdb.list(args.filter_by_stream, args.filter_by_year, args.filter_by_field)
+        print(json.dumps(docs, indent=args.indent))
+    elif args.op == 'get':
+        doc = tdb.get(args.id)
+        print(json.dumps(doc, indent=args.indent))
+    elif args.op == 'update':
+        if args.set:
+            tdb.update(args.id, args.set[0], args.set[1], action='set')
+        if args.add:
+            tdb.update(args.id, args.add[0], args.add[1])
+        if args.remove:
+            tdb.update(args.id, args.remove[0], args.remove[1], action='remove')
+        if args.inc:
+            tdb.update(args.id, args.inc[0], args.inc[1], action='remove')
+    else:
+        raise Exception("Not implemented!")
 
 
 if __name__ == "__main__":
--- lib/trackdb/solr.py
+++ lib/trackdb/solr.py
@@ -13,28 +13,25 @@ logger = logging.getLogger(__name__)
 
 class SolrTrackDB():
 
-    # Some optional filters:
-    stream = None # Could filter to e.g. 'frequent'
-    year = None # Could filter to e.g. 2020
-
     def __init__(self, trackdb_url, kind='warcs'):
         self.trackdb_url = trackdb_url
         self.kind = kind
 
-    def list(self, stream, year, field, value, sort='timestamp_dt desc', limit=100):
+    def list(self, stream=None, year=None, field_value=None, sort='timestamp_dt desc', limit=100):
         # set solr search terms
         solr_query_url = self.trackdb_url + '/query'
         query_string = {
-            'q':'kind_s:{} AND {}:{}'.format(
-                self.kind, field, value),
+            'q':'kind_s:{}'.format(self.kind),
             'rows':limit,
             'sort':sort
         }
         # Add optional fields:
-        if self.stream:
-            query_string['q'] += ' AND stream_s:"%s"' % self.stream
-        if self.year:
-            query_string['q'] += ' AND year_i:"%s"' % self.year
+        if stream:
+            query_string['q'] += ' AND stream_s:%s' % stream
+        if year:
+            query_string['q'] += ' AND year_i:%s' % year
+        if field_value:
+            query_string['q'] += ' AND {}'.format(field_value)
         # gain tracking_db search response
         logger.warn("SolrTrackDB.list: %s %s" %(solr_query_url, query_string))
         r = requests.post(url=solr_query_url, data=query_string)
@@ -48,13 +45,33 @@ class SolrTrackDB():
         else:
             raise Exception("Solr returned an error! HTTP %i\n%s" %(r.status_code, r.text))
 
-    def update(self, id, field, value):
+    def get(self, id):
+        # set solr search terms
+        solr_query_url = self.trackdb_url + '/query'
+        query_string = {
+            'q':'kind_s:{} AND id:"{}"'.format(self.kind, id)
+        }
+        # gain tracking_db search response
+        logger.info("SolrTrackDB.get: %s %s" %(solr_query_url, query_string))
+        r = requests.post(url=solr_query_url, data=query_string)
+        if r.status_code == 200:
+            response = r.json()['response']
+            # return hits, if any:
+            if response['numFound'] == 1:
+                return response['docs'][0]
+            else:
+                return None
+        else:
+            raise Exception("Solr returned an error! HTTP %i\n%s" %(r.status_code, r.text))
+
+    def update(self, id, field, value, action='add-distinct'):
         # Update trackdb record for warc
         update_trackdb_url = self.trackdb_url + '/update?commitWithin=1000'
         post_headers = {'Content-Type': 'application/json'}
-        post_data = { 'id': id, field: {'add-distinct': value} }
+        post_data = { 'id': id, field: { action: value} }
 
         # gain tracking_db search response
+        logger.info("SolrTrackDB.update: %s %s" %(update_trackdb_url, post_data))
         r = requests.post(url=update_trackdb_url, headers=post_headers, json=[post_data])
         if r.status_code == 200:
             response = r.json()
