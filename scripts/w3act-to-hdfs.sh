32eff35e678f654d84be7574c2a7b621dceb46bf
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-04-23 10:47:55 +0100

    Add exit-on-fail logic and fix store command.

--- scripts/w3act-to-hdfs.sh
+++ scripts/w3act-to-hdfs.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 #
-# 
+# Run with the correct environment, this will backup the JSON from W3ACT to HDFS.
+#
+# e.g.
+#
+#     docker run -ti -e W3ACT_PSQL_PASSWORD --network w3act_default ukwa/ukwa-manage w3act-to-hdfs.sh
+#
+
+# Exit when any simple command or piped command fails:
+set -e -o pipefail
 
 # Make a folder for the CSV dump:
 mkdir w3act-db-csv
+
+# Download the CSV dump of the W3ACT database:
 w3act-csv -H postgres get-csv
 
-# Convert to JSON and GZIP:
+# Convert to JSON:
 w3act-csv csv-to-json
+
+# ...and GZIP it:
 gzip w3act-db-csv.json
 
 # Upload to HDFS, using the 'ingest' user account:
-store put -u ingest w3act-db-csv.json.gz /9_processing/w3act
+store -u ingest put w3act-db-csv.json.gz /9_processing/w3act
