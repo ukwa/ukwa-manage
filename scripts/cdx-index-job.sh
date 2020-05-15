50a83baaf5ff7aabdd40a7faa5fd07ad178e8c94
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-05-15 15:34:14 +0100

    Added updated scripts to run TrackDB commands.

--- scripts/cdx-index-job.sh
+++ scripts/cdx-index-job.sh
@@ -5,17 +5,27 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-export JOB_INPUT=/9_processing/warcs2cdx/warcs-list.txt
-export JOB_OUTPUT=/9_processing/warcs2cdx/output
+# Get the folder this script is in:
+DIR="$(dirname "$(readlink -f "$0")")"
 
+# If not already set, setup path to the job JAR:
+if [[ -z "${JOB_JAR}" ]]; then
+    JOB_JAR=${DIR}/warc-hadoop-recordreaders-job.jar
+fi
+
+# Set up job input/output
+JOB_INPUT=/9_processing/warcs2cdx/warcs-list.txt
+JOB_OUTPUT=/9_processing/warcs2cdx/output
+
+# Remove old input/output:
 hadoop fs -rm $JOB_INPUT
 hadoop fs -rmr $JOB_OUTPUT
 
-# Upload as input:
+# Upload input file:
 hadoop fs -put $1 $JOB_INPUT
 
 # Run the job (using 5 reducers):
-hadoop jar /host/tasks/jars/warc-hadoop-recordreaders-3.1.0-SNAPSHOT-job.jar \
+hadoop jar $JOB_JAR \
     uk.bl.wa.hadoop.mapreduce.cdx.ArchiveCDXGenerator \
     -Dmapred.compress.map.output=true \
     -Dmapred.output.compress=true \
---
+++ scripts/trackdb-run-cdx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Important line that stops the script continuing if part of the job fails!
+set -e
+
+# Parameters:
+YEAR="2020"
+STREAM="frequent"
+CDX_COLLECTION="data-heritrix"
+CDX_SERVER="http://cdx.dapi.wa.bl.uk/${CDX_COLLECTION}"
+TRACKDB_URL="http://trackdb.dapi.wa.bl.uk/solr/tracking"
+
+# Get the folder this script is in:
+DIR="$(dirname "$(readlink -f "$0")")"
+
+# Get a list of WARCs to process from the TrackDB:
+echo "Listing WARCs..."
+trackdb warcs --stream $STREAM --year $YEAR --field cdx_index_ss _NONE_ list --limit 1000 --ids-only > warcs-to-index.ids
+
+# Run the Hadoop job using this list:
+echo "Running Hadoop job..."
+${DIR}/cdx-index-job.sh warcs-to-index.ids ${CDX_SERVER}
+
+# If all is well, update the TrackDB (skipping verification for now):
+echo "Marking WARCs as indexed..."
+cat warcs-to-index.ids | trackdb warcs update --set cdx_index_ss ${CDX_COLLECTION} -
+
