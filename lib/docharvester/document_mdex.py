b9d39955d48481d826711644a1fd3313beae3993
Andrew Jackson <Andrew.Jackson@bl.uk>
2022-11-08 17:07:33 +0000

    Also timeout HEAD request, for #104.

--- lib/docharvester/document_mdex.py
+++ lib/docharvester/document_mdex.py
@@ -203,7 +203,7 @@ class DocumentMDEx(object):
         tries = 5
         success = False
         while tries > 0:
-            r = requests.head(url=self.doc_wb_url(), allow_redirects=True)
+            r = requests.head(url=self.doc_wb_url(), allow_redirects=True, timeout=TIMEOUT)
             if 'up' in r.links:
                 lpu = r.links['up']
                 self.doc["landing_page_url"] = lpu['url']
